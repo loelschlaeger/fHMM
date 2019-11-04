a47bce6e40647a53036922c96b00bffa4b43fb8b
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2019-11-04 17:21:06 +0100

    Changed from relative to absolute error

--- plotEstimationError.R
+++ plotEstimationError.R
@@ -4,14 +4,14 @@ plotEstimationError <- function(theta,est,controls) {
   N = controls[["N"]]
   est_df     = controls[["est_df"]]
 	
-	diff_gamma = (theta[["gammas"]]-est[["gammas"]]) / theta[["gammas"]]
-	diff_mu    = (theta[["mus"]]-est[["mus"]])       / theta[["mus"]]
-	diff_sigma = (theta[["sigmas"]]-est[["sigmas"]]) / theta[["sigmas"]]
-	diff_df    = (theta[["dfs"]]-est[["dfs"]])       / theta[["dfs"]]
+	diff_gamma = abs(theta[["gammas"]]-est[["gammas"]]) 
+	diff_mu    = abs(theta[["mus"]]-est[["mus"]])       
+	diff_sigma = abs(theta[["sigmas"]]-est[["sigmas"]]) 
+	diff_df    = abs(theta[["dfs"]]-est[["dfs"]])       
 	
 	par(mfrow=c(2,2))
 	xlab = "Parameter Index"
-	ylab = "Relative Error"
+	ylab = "Absolute Error"
 	
 	plot(diff_gamma,xlab=xlab,ylab=ylab,main="Gamma")
 	plot(diff_mu,xlab=xlab,ylab=ylab,main="Mu")
