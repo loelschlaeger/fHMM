89d5cd2d2955a8ea5e10a3abd7b3b44516ff801e
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2019-11-11 17:26:53 +0100

    Update plotEstimationError.R

--- plotEstimationError.R
+++ plotEstimationError.R
@@ -1,17 +1,17 @@
-plotEstimationError <- function(theta,est,controls) {
+plotEstimationError = function(theta,est,controls) {
   
   M = controls[["M"]]
   N = controls[["N"]]
   est_df     = controls[["est_df"]]
 	
-	diff_gamma = abs(theta[["gammas"]]-est[["gammas"]]) 
-	diff_mu    = abs(theta[["mus"]]-est[["mus"]])       
-	diff_sigma = abs(theta[["sigmas"]]-est[["sigmas"]]) 
-	diff_df    = abs(theta[["dfs"]]-est[["dfs"]])       
+	diff_gamma = theta[1:(M*(M-1)+M*N*(N-1))]-est[1:(M*(M-1)+M*N*(N-1))]; theta = theta[-(1:(M*(M-1)+M*N*(N-1)))]; est = est[-(1:(M*(M-1)+M*N*(N-1)))]
+	diff_mu    = theta[1:(M+M*N)]-est[1:(M+M*N)]; theta = theta[-(1:(M+M*N))]; est = est[-(1:(M+M*N))]    
+	diff_sigma = theta[1:(M+M*N)]-est[1:(M+M*N)]; theta = theta[-(1:(M+M*N))]; est = est[-(1:(M+M*N))]  
+	if(est_df=="all" || est_df=="fscs") {diff_df    = theta-est}     
 	
 	par(mfrow=c(2,2))
-	xlab = "Parameter Index"
-	ylab = "Absolute Error"
+	xlab = "parameter index"
+	ylab = "true minus estimate"
 	
 	plot(diff_gamma,xlab=xlab,ylab=ylab,main="Gamma")
 	plot(diff_mu,xlab=xlab,ylab=ylab,main="Mu")
