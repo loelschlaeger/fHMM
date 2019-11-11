852c5ffb6dbe94e6aa17af235fe3b34d46530ced
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2019-11-11 17:26:59 +0100

    Update maxLikelihood.R

--- maxLikelihood.R
+++ maxLikelihood.R
@@ -2,25 +2,23 @@
 
 source("computeLikelihood.R")
 
-#install.packages("Rcpp")
-#install.packages("RcppArmadillo")
 library(Rcpp)
 library(RcppArmadillo)
 sourceCpp("P:/HHMMFinance/RHHMMFinance/LogLike.cpp") # C++ script required for the likelihood evaluation 
 
-source("transformParameters.R")
+source("transformations.R")
 source("initializeEstimation.R")
-source("createOutputFile.R")
 
-maxLikelihood <- function(observations,controls){
+maxLikelihood = function(observations,controls){
   
   M          = controls[["M"]]
   N          = controls[["N"]]
   runs       = controls[["runs"]]
-  output     = controls[["output"]]
   iterlim    = controls[["iterlim"]]
+  outputFile = controls[["outputFile"]]
   
   cat("Start with estimation. This will take some time.\n")
+  cat("0%...")
 
 	llks = rep(NA,runs) 
 	mods = list() 
@@ -28,26 +26,32 @@ maxLikelihood <- function(observations,controls){
 	for (k in 1:runs){ 
 		tryCatch({
 			start = initializeEstimation(controls) 
-			mods[[k]] = nlm(f=logL_hhmm,p=start,observations=observations,controls=controls,iterlim=iterlim,steptol = 1e-10)
+			mods[[k]] = nlm(f=logL_hhmm,p=start,observations=observations,controls=controls,iterlim=iterlim,steptol = 1e-08,print.level=0)
 			llks[k] = mods[[k]]$minimum
 			},
-			error=function(e){cat("Fehler in diesem Durchgang:",conditionMessage(e), "\n")}
+			error=function(e){cat("(Fehler:",conditionMessage(e), ")")}
 			)
-		cat(paste(round(k/runs*100,1),"%\n"))
+		if(k<runs){cat(paste0(round(k/runs*100,1),"%..."))}
+	  if(k==runs){cat("100%\n")}
 	}
-	cat(paste("Done with estimation. Computation time:", Sys.time()-t1,"\n"))
+	t2 = Sys.time()
+	cat(paste("Done with estimation.", difftime(t2,t1),"\n"))
 	
-	mod = mods[[which.min(llks)]]
-	est = mod$estimate
-	est = backTransform(est,controls)
+	mod       = mods[[which.min(llks)]]
+	thetaCon  = thetaUncon2thetaCon(mod$estimate,controls)
+	thetaFull = thetaCon2thetaFull(thetaCon,controls)
 	
 	cat(paste("Iterations:",mod$iterations,"(if close to iteration limit, consider to increase)\n "))
-	cat("Gradient:\n")
-	print(mod$gradient)
-	cat("Estimates:\n")
-	print(est)
+	
+	if(outputFile){
+	  source("createOutputFile.R")
+	}
 
-	if(output){createOutputFile(est,controls)}
-
-	return(est)
+	return(list(
+	  "likelihood"= -mod$minimum,
+	  "thetaCon"  = thetaCon,
+	  "thetaFull" = thetaFull,
+	  "gradient"  = mod$gradient
+	  )  
+	)
 }
