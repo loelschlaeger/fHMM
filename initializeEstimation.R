07e144ce651e23c8ab0b5f9656715e3cb8126ba7
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2019-11-11 17:25:58 +0100

    Update initializeEstimation.R

--- initializeEstimation.R
+++ initializeEstimation.R
@@ -1,19 +1,20 @@
-initializeEstimation <- function(controls,atTrueValues=FALSE){
+# Initializes randomly the estimation routine
+
+initializeEstimation = function(controls){
   
   M          = controls[["M"]]
   N          = controls[["N"]]
   est_df     = controls[["est_df"]]
-  set_df_cs  = controls[["set_df_cs"]] 
-  set_df_fs  = controls[["set_df_fs"]] 
   
-  gammas = runif((M-1)*M+M*(N-1)*N,-2,-1) #non-diagonal entries of tpm
-  mus    = rnorm(M+M*N)*10^(-3)
-  sigmas = log(runif(M+M*N,0.1,1)*10^(-2))
-  if(est_df=="all") {dfs     = runif(M+M*N,0,0.5)}
-  if(est_df=="fscs"){dfs     = runif(2,0,0.5)}
-  if(est_df=="no")  {dfs     = c()}
+  gammasUncon     = runif((M-1)*M+M*(N-1)*N,-2,-1) #non-diagonal entries of tpm
+  musUncon        = rnorm(M+M*N)*10^(-3)
+  sigmasUncon     = log(runif(M+M*N,0.1,1)*10^(-2))
   
-  start <- c(gammas,mus,sigmas,dfs)
+  if(est_df=="all") {dfsUncon     = runif(M+M*N,0,0.5)}
+  if(est_df=="fscs"){dfsUncon     = runif(2,0,0.5)}
+  if(est_df=="no")  {dfsUncon     = c()}
   
-  return(start)
+  thetaUncon = c(gammasUncon,musUncon,sigmasUncon,dfsUncon)
+  
+  return(thetaUncon)
 }
