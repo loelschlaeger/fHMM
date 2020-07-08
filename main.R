53b641deb33130a9f5c014cf7d8598f82756a7de
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2020-07-08 17:37:05 +0200

    save estimates in txt file

--- main.R
+++ main.R
@@ -5,21 +5,13 @@ rm(list = ls()); cat("\f")
 if(!dir.exists("models")){dir.create("models")}
 
 ### 3. Load old model
-<<<<<<< HEAD
-path = "models/HHMM_SandP_32_longrun"
-=======
-path = "models/HHMM_DAX_32"
->>>>>>> 8b671458259d7b9ceb40de104e69f8927ab7433b
-if(file.exists(path)){load(file=path)}
+path = "models/HHMM_sandp500_32_longrun"
+if(file.exists(path)){load(file = path)}
 
 ### 4. Set model parameters for new model
 controls = list(
-<<<<<<< HEAD
-  modelName = "HHMM_DAX_32_longrun",
-=======
-  modelName = "HHMM_DAX_32",
->>>>>>> 8b671458259d7b9ceb40de104e69f8927ab7433b
-  fileName  = "data/dax.csv", 
+  modelName = "HHMM_sandp500_32_longrun",
+  fileName  = "data/sandp500.csv", 
   M         = 3,
   N         = 2,
   est_df    = "yes",
@@ -27,34 +19,30 @@ controls = list(
   set_df_fs = 1,       
   T         = 100,                     
   T_star    = 30,
-  t_min     = "2000-1-3", 
-  t_max     = "2020-05-28",     
-<<<<<<< HEAD
+  t_min     = "2000-01-03", 
+  t_max     = "2020-01-30",     
   runs      = 1000,
   iterlim   = 500
-=======
-  runs      = 200,
-  iterlim   = 1000
->>>>>>> 8b671458259d7b9ceb40de104e69f8927ab7433b
 )
 
 ### 5a. Fit model to simulated data
 source("data_sim.R")
 sim = simulateHHMM(controls)
 source("optim.R") 
-est_sim = maxLikelihood(sim[["observations"]],controls)
-save(sim,controls,est_sim,file=paste0("models/",controls[["modelName"]]))
+est_sim = maxLikelihood(sim[["observations"]], controls)
+save(sim, controls, est_sim, file=paste0("models/", controls[["modelName"]]))
 
 ### 5b. Fit model to empirical data and process estimates
 source("data.R")
 data = readData(controls)
 source("optim.R") 
-est = maxLikelihood(data[["observations"]],controls)
+est = maxLikelihood(data[["observations"]], controls)
+sink(file = paste0("models/",controls$modelName,"_estimates.txtf")); controls; est; sink()
 source("viterbi.R")
-states = applyViterbi(data[["observations"]],est[["thetaFull"]],controls)
-save(data,controls,est,states,file=paste0("models/",controls[["modelName"]]))
+states = applyViterbi(data[["observations"]], est[["thetaFull"]], controls)
+save(data, controls, est, states, file = paste0("models/", controls[["modelName"]]))
 
-### 6. Plot graphics (M=3, N=2)
+### 6. Plot graphics (only M=3 and N=2)
 source("plots.R")
-hhmm_visul(data,est,states)
+hhmm_visual(data, est, states, controls)
 
