8599c3eb4940430389fbb2559dba84970c731481
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2020-11-11 20:56:15 +0100

    included a seed, mus and sigmas are now sorted (highest mean return and lowest variance at the first place, lowest mean return and highest variance at the last place)

--- init.R
+++ init.R
@@ -1,17 +1,19 @@
 ### initializes the estimation routine randomly
+source("trans.R")
 
 init_est = function(controls){
+  set.seed(controls[["seed"]])
   M  = controls[["states"]][1] #coarse-scale states
   N  = controls[["states"]][2] #fine-scale states
   df_cs = controls[["fix_df"]][1]
   df_fs = controls[["fix_df"]][2]
   
   gammasUncon = gammasCon2gammasUncon(runif((M-1)*M,0,1/M),M)
-  mus         = rnorm(M)*10^(-3)
-  sigmasUncon = log(runif(M,0.1,1)*10^(-2))
+  mus         = sort(rnorm(M)*10^(-3),decreasing=TRUE)
+  sigmasUncon = log(sort(runif(M,0.1,1)*10^(-2),decreasing=FALSE))
   dfs         = if(is.na(df_cs)) sample(1:30,M,replace=TRUE) else integer(0)
-  for(n in seq_len(N)){
-    gammasUncon = c(gammasUncon,gammasCon2gammasUncon(runif((N-1)*N,0,1/N),N),M)
+  for(m in seq_len(M)){
+    gammasUncon = c(gammasUncon,gammasCon2gammasUncon(runif((N-1)*N,0,1/N),N))
     mus         = c(mus,rnorm(N)*10^(-3))
     sigmasUncon = c(sigmasUncon,log(runif(N,0.1,1)*10^(-2)))
     dfs         = c(dfs,if(is.na(df_fs)) sample(1:30,N,replace=TRUE) else integer(0))
