2a300de73f0070c305d5b4805ab32d135d35e894
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2020-11-21 11:24:26 +0100

    extended check functions

--- checks.R
+++ checks.R
@@ -1,6 +1,6 @@
-### checks control parameters
-
 check_controls = function(controls){
+  attach(controls)
+  on.exit(detach(controls))
   required_controls = c("modelName","states","timeHorizon")
   for(required_control in required_controls){
     if(!(required_control %in% names(controls))) stop(paste0("Please specify '", required_control, "' in 'controls'."),call.=FALSE)
@@ -8,17 +8,17 @@ check_controls = function(controls){
   all_controls = c("modelName","dataSource","trunc_data","states","timeHorizon","fix_df","runs","iterlim","hessian","seed")
   missing_controls = setdiff(all_controls,names(controls))
   for(missing_control in missing_controls){
-    warning(paste("The following control is not specified and set to default:", missing_control))
+    warning(paste("The following element in 'control' is not specified and set to default:", missing_control),call.=FALSE)
   }
   if("dataSource" %in% missing_controls) controls[["dataSource"]] = c(NA,NA)
   if("trunc_data" %in% missing_controls) controls[["trunc_data"]] = c(NA,NA)
   if("fix_df" %in% missing_controls) controls[["fix_df"]] = c(NA,NA)
   if("runs" %in% missing_controls) controls[["runs"]] = 200
   if("iterlim" %in% missing_controls) controls[["iterlim"]] = 500
-  if("hessian" %in% missing_controls) controls[["hessian"]] = FALSE
+  if("hessian" %in% missing_controls) controls[["hessian"]] = TRUE
   if("seed" %in% missing_controls) controls[["seed"]] = NULL
   for(redundant_control in setdiff(names(controls),all_controls)){
-    warning(paste("The following control is not supported and will be ignored:", redundant_control))
+    warning(paste("The following element in 'control' is not supported and will be ignored:", redundant_control),call.=FALSE)
   }
   controls_with_length_2 = c("dataSource","trunc_data","states","timeHorizon")
   for(control_with_length_2 in controls_with_length_2){
@@ -28,30 +28,37 @@ check_controls = function(controls){
   for(numeric_control in numeric_controls){
     if(!is.numeric(controls[[numeric_control]])) stop(paste0("'", numeric_control, "' in 'controls' must be numeric."),call.=FALSE)
   }
+  if(is.na(controls[["dataSource"]][1])) controls[["sim"]] = TRUE
+  if(!is.na(controls[["dataSource"]][1])) controls[["sim"]] = FALSE
+  if(controls[["states"]][2]==0) {
+    controls[["model"]] = "HMM"
+    writeLines("Model: HMM")
+    writeLines(paste("States:",controls[["states"]][1]))
+    writeLines("State-dependent distributions: t-distributions",sep=" ")
+    if(is.na(controls[["fix_df"]][1])) writeLines("")
+    if(!is.na(controls[["fix_df"]][1])) writeLines(paste0("(degrees of freedom fixed to ",controls[["fix_df"]][1],")"))
+  } else {
+    controls[["model"]] = "HHMM"
+    writeLines("Model: HHMM")
+    writeLines(paste("States:",states[1],"/",states[2]))
+    writeLines("State-dependent distributions: t-distributions",sep=" ")
+    if(is.na(controls[["fix_df"]][1])) writeLines("")
+    if(!is.na(controls[["fix_df"]][1])) writeLines(paste0("(degrees of freedom fixed to ",controls[["fix_df"]][1]," / ",controls[["fix_df"]][2],")"))
+  }
   return(controls)
 }
 
-# TODO: extend for HHMM
-check_model = function(controls){
-  attach(controls)
-  on.exit(detach(controls))
-  if(states[2]==0) {
-    writeLines("Model: HMM")
-    writeLines(paste("States:",states[1]))
-    writeLines(paste("Time horizon:",timeHorizon[1]))
-    writeLines("State dependent distributions: t-distributions",sep=" ")
-    if(is.na(fix_df[1])) writeLines(paste("Degrees :",timeHorizon[1]))
-  } else { 
-    writeLines("Model: HHMM")
+# TODO: truncation?, timeHorizon still valid?
+check_data = function(controls,data){
+  if(controls[["sim"]]){
+    writeLines("Data: simulated")
+  }
+  if(!controls[["sim"]]){
+    writeLines("Data: empirical")
   }
 }
 
-# TODO: sim or emp data, truncation?, timeHorizon still valid?
-check_data = function(controls){
-  return(TRUE)
-}
-
-# TODO: runs, iterlim, hessian, seed
+# TODO: runs?, iterlim to close at true iterations?, hessian computed?, seed
 check_estimation = function(controls){
 
 }
