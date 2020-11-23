a1f58cb30c500547779f826914361568519e35c3
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2020-11-23 17:20:25 +0100

    put max. of LL for HMM and HHMM together

--- optim.R
+++ optim.R
@@ -1,48 +1,43 @@
-# maximization of the likelihood function
+### maximization of the likelihood function
 
-## required packages
 library(Rcpp)
 library(RcppArmadillo)
-
-sourceCpp("loglike_cpp.cpp")
+library(progress)
+sourceCpp("loglike.cpp")
 source("loglike.R")
 source("trans.R")
 source("init.R")
+source("checks.R")
 
-maxLikelihood = function(observations,controls){
-  M          = controls[["M"]]
-  N          = controls[["N"]]
-  runs       = controls[["runs"]]
-  iterlim    = controls[["iterlim"]]
-  hessian    = FALSE # hessian = controls[["hessian"]]
-  cat("Start likelihood maximization\n")
-  cat("run (estimated time remaining in min)\n")
-	llks = rep(NA,runs) 
-	mods = list() 
-	
-	start_time = Sys.time()
+maxLikelihood = function(data,controls){
+  observations = data[["observations"]]
+  runs = controls[["runs"]]
+  llks = rep(NA,runs) 
+  mods = list() 
+  if(controls[["model"]]=="HMM") target = nLL_hmm
+  if(controls[["model"]]=="HHMM") target = nLL_hhmm
+  
+  writeLines("Started estimation.")
+  pb = progress_bar$new(format = "[:bar] :percent eta: :eta", total = runs, clear = TRUE)
+  start = Sys.time()
 	for (k in 1:runs){
-	  start = init_est(controls) 
-		tryCatch({
-			  mods[[k]] = nlm(f=logL_hhmm,p=start,observations=observations,controls=controls,iterlim=iterlim,steptol=1e-08,print.level=0,hessian=hessian)
-			  llks[k] = mods[[k]]$minimum
-			},error=function(e){cat(paste("(Error:",conditionMessage(e),")"),"\n")}
-		)
-    if(k%%10==0) cat(paste0(k," of ",runs," (",round(difftime(Sys.time(),start_time,units="mins")*(runs-k)/k),")","\n"))
+	  suppressWarnings({ tryCatch({ mods[[k]] = nlm(f = target,
+    			                                        p = init_est(controls),
+                            			                observations = observations,
+                            			                controls = controls,
+                            			                iterlim = controls[["iterlim"]],
+                            			                steptol = controls[["steptol"]],
+                            			                print.level = controls[["print.level"]],
+                            			                hessian = controls[["hessian"]]
+    			                                        )
+    		                          if(mods[[k]]$code %in% controls[["accept_codes"]]) llks[k] = mods[[k]]$minimum
+  			                        },error = function(e){})
+  		              })
+	  pb$tick()
 	}
-	end_time = Sys.time()
-	cat(paste("Done with estimation.", round(difftime(end_time,start_time,units="mins")),"mins total estimation time. \n"))
+  end = Sys.time()
+  
+	est = check_estimation(round(difftime(end,start,units='mins')),mods,llks,data,controls)
 	
-	mod       = mods[[which.min(llks)]]
-	thetaCon  = thetaUncon2thetaCon(mod$estimate,controls)
-	thetaFull = thetaCon2thetaFull(thetaCon,controls)
-	
-	return(list(
-	  "likelihood"      = -mod$minimum,
-	  "all_likelihoods" = -llks,
-	  "thetaFull"       = thetaFull,
-	  "gradient"        = mod$gradient,
-          "hessian"         = mod$hessian
-	  )  
-	)
+	return(est)
 }
