9b5fa74d7c42701b91f61964ae79513992f19d0c
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2020-11-23 17:21:45 +0100

    created two functions for init: 1) init of new code and 2) init of old code

--- init.R
+++ init.R
@@ -1,4 +1,14 @@
-### initializes the estimation routine randomly
+### initialize the code
+init = function(){
+  cat("\f")
+  writeLines("Fitting (hierarchical) hidden Markov models to financial data.")
+  if(!dir.exists("models")){
+    dir.create("models")
+  }
+  writeLines(paste0("Estimation results will be saved in '",getwd(),"/models'."))
+}
+
+### initialize the estimation routine randomly
 source("trans.R")
 
 init_est = function(controls){
@@ -8,16 +18,35 @@ init_est = function(controls){
   df_fs = controls[["fix_df"]][2]
   
   gammasUncon = gammasCon2gammasUncon(runif((M-1)*M,0,1/M),M)
-  mus         = sort(rnorm(M)*10^(-3),decreasing=TRUE)
+  mus         = sort(rnorm(M)*10^(-1),decreasing=TRUE)
   sigmasUncon = log(sort(runif(M,0.1,1)*10^(-2),decreasing=FALSE))
-  dfs         = if(is.na(df_cs)) sample(1:30,M,replace=TRUE) else integer(0)
-  if(N!=0) for(m in seq_len(M)){
+  dfs         = if(is.na(df_cs)) runif(M,0,30) else integer(0)
+  if(controls[["model"]]=="HHMM") for(m in seq_len(M)){
     gammasUncon = c(gammasUncon,gammasCon2gammasUncon(runif((N-1)*N,0,1/N),N))
-    mus         = c(mus,rnorm(N)*10^(-3))
+    mus         = c(mus,rnorm(N)*10^(-1))
     sigmasUncon = c(sigmasUncon,log(runif(N,0.1,1)*10^(-2)))
-    dfs         = c(dfs,if(is.na(df_fs)) sample(1:30,N,replace=TRUE) else integer(0))
+    dfs         = c(dfs,if(is.na(df_fs)) runif(N,0,30) else integer(0))
   }
   
   thetaUncon = c(gammasUncon,mus,sigmasUncon,dfs)
   return(thetaUncon)
+}
+
+#TODO: check if something gets overwritten
+### load parameters and results of the old model 'name'
+loadModel = function(name){
+  loadable = c("controls","data","est","states")
+  path = paste0("models/",name)
+  if(!dir.exists(path)){
+    stop(paste0("Path '",path,"' does not exist."),call.=FALSE)
+  }
+  loaded = list()
+  for(object in loadable){
+    if(file.exists(paste0(path,"/",object))){
+      loaded[[object]] = readRDS(paste0(path,"/",object))
+    }
+  }
+  if(length(loaded)>=1) writeLines(paste0("Loaded ",paste0("'",names(loaded),"'",collapse=", ")," from model '",name,"'."))
+  if(length(loaded)==0) writeLines(paste0("Unable to load any object from path '",path,"'."))
+  invisible(list2env(loaded, envir = .GlobalEnv))
 }
