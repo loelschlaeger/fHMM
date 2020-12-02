345327b170f24dcbb2d483f65ef5b07ad8a1a4cc
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2020-12-02 15:49:27 +0100

    minor

--- main.R
+++ main.R
@@ -5,13 +5,12 @@ source("init.R"); init()
 ### 2. Set and check controls
 controls = list(
   model_name    = "test",        
-  data_source   = c(NA,NA),
-  truncate_data = c("2001-01-03","2020-01-30"), 
-  states        = c(2,2),
+  data_source   = c("sandp500.csv","dax.csv"),
+  #truncate_data = c("2001-01-03","2020-01-30"), 
+  states        = c(3,2),
   time_horizon  = c(50,5),
   fix_dfs       = c(NA,NA),
-  runs          = 50,
-  iterlim       = 200,
+  runs          = 2,
   #seed          = 1,
   overwrite     = TRUE
 )
@@ -29,7 +28,7 @@ labels = list(
   dates = c("2008-9-15"),
   names = c("bankruptcy of Lehman Brothers")
 )
-visual(data,fit,decoding,labels,controls)
+visual(data,fit,decoding,controls,labels)
 
 ### 6. Reinitialize model
 reinit("test")
