348c10526a938af7bdc65dc876868ecd1ebd2c3c
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2020-12-10 18:36:27 +0100

    visualization of decoded ts for any no of states

--- visual.R
+++ visual.R
@@ -1,10 +1,26 @@
 ### creates graphics of model results
 
 visual = function(data,fit,decoding,controls,labels=NULL){
-  if(is.null(controls[["controls_checked"]])) stop("'controls' invalid",call.=FALSE)
-  if(controls[["sim"]] & !is.null(labels)) warning("Entries of 'labels' will be ignored since 'data' was simulated.",call.=FALSE)
   
+  ### pre-checks
+  if(is.null(controls[["controls_checked"]])) stop("'controls' invalid",call.=FALSE)
+  if(controls[["sim"]] & !is.null(labels)){
+    warning("Entries of 'labels' will be ignored since 'data' was simulated.",call.=FALSE)
+  } else if(!controls[["sim"]] & !is.null(labels) & length(labels[["dates"]])!=length(labels[["names"]])){
+    stop("'dates' and 'names' in 'labels' must be of the same length.")
+  }
+  
+  ### extract parameters
   states = controls[["states"]]
+  if(controls[["model"]]=="HMM"){
+    T = length(data$observations)
+  }
+  if(controls[["model"]]=="HHMM"){
+    T = dim(data$observations)[1]
+    T_star = dim(data$observations)[2]-1
+    decoding_cs = rep(decoding[,1],T_star)
+    decoding_fs = as.vector(t(decoding[,-1]))
+  }
   attach(fit[["thetaList"]])
   on.exit(detach(fit[["thetaList"]]))
   
@@ -16,6 +32,9 @@ visual = function(data,fit,decoding,controls,labels=NULL){
   colors[,1] = col.alpha(base.col(states[1]))
   for(s in seq_len(states[1])) colors[s,-1] = col.alpha(var.col(colors[s,1],states[2]))
   
+  ### define layout
+  legend_layout = list(cex=1.25,x="topleft",bg="white")
+  
   ### state dependent distributions
   if(controls[["model"]]=="HMM"){
     pdf(paste0("models/",controls[["model_name"]],"/sdd.pdf"), width=9, height=7)
@@ -26,7 +45,7 @@ visual = function(data,fit,decoding,controls,labels=NULL){
     hist(data$observations,prob=TRUE,xlim=c(xmin,xmax),ylim=c(ymin,ymax),col="white",border="white",xaxt="n",yaxt="n",xlab="",ylab="",main="")
     axis(1,seq(xmin,xmax,by=0.1)); axis(2,seq(ymin,ymax,by=20))
     title(main="State-dependent distributions",xlab="log-return",ylab="density")
-    legend("topleft",legend=paste("state",seq_len(states[1])),col=colors[,1],lwd=lwd,pt.cex=1,cex=1.5)
+    do.call(legend,c(list(legend=paste("state",seq_len(states[1])),col=colors[,1],lwd=3),legend_layout))
     for(s in seq_len(states[1])){
       lines(x,sdd(s,x),col=colors[s,1],lwd=lwd)
     }
@@ -42,7 +61,7 @@ visual = function(data,fit,decoding,controls,labels=NULL){
     hist(data$observations[,1],prob=TRUE,xlim=c(xmin,xmax),ylim=c(ymin,ymax),col="white",border="white",xaxt="n",yaxt="n",xlab="",ylab="",main="")
     axis(1,seq(xmin,xmax,by=0.1)); axis(2,seq(ymin,ymax,by=40))
     title(main="State-dependent distributions",xlab="log-return",ylab="density")
-    legend("topleft",legend=paste("coarse-scale state",seq_len(states[1])),col=colors[,1],lwd=lwd,pt.cex=1,cex=1.5)
+    do.call(legend,c(list(legend=paste("coarse-scale state",seq_len(states[1])),col=colors[,1],lwd=3),legend_layout))
     for(s in seq_len(states[1])){
       lines(x,sdd(s,x),col=colors[s,1],lwd=lwd)
     }
@@ -55,7 +74,7 @@ visual = function(data,fit,decoding,controls,labels=NULL){
       hist(as.vector(data$observations[,-1]),prob=TRUE,xlim=c(xmin,xmax),ylim=c(ymin,ymax),col="white",border="white",xaxt="n",yaxt="n",xlab="",ylab="",main="")
       axis(1,seq(xmin,xmax,by=0.1)); axis(2,seq(ymin,ymax,by=20))
       title(main=paste("State-dependent distributions conditional on coarse-scale state",cs),xlab="log-return",ylab="density")
-      legend("topleft",legend=paste("fine-scale state",seq_len(states[2])),col=colors[cs,-1],lwd=lwd,pt.cex=1,cex=1.5)
+      do.call(legend,c(list(legend=paste("fine-scale state",seq_len(states[2])),col=colors[cs,-1],lwd=3),legend_layout))
       for(fs in seq_len(states[2])){
         lines(x,sdd(cs,fs,x),col=colors[cs,fs+1],lwd=lwd)
       }
@@ -63,60 +82,78 @@ visual = function(data,fit,decoding,controls,labels=NULL){
     invisible(dev.off())
   }
   
-  if(FALSE){
   ### decoded time series
-  pdf(paste0("models/",controls$modelName,"_decoded_ts.pdf"), width=19, height=9)
+  pdf(paste0("models/",controls[["model_name"]],"/ts.pdf"), width=19, height=9)
   par(mfrow=c(1,1),las=1,mar=c(4,6,0.5,6))
-  xmin = as.Date(controls$t_min)
-  xmax = as.Date(controls$t_max)
-  ymin = floor(min(close)/1000)*1000 - (ceiling(max(close)/1000)*1000-floor(min(close)/1000)*1000)/1.5
-  ymax = max(close)#ceiling(max(close)/1000)*1000
-  plot(date,close,type="l",xlim=c(xmin,xmax),ylim=c(ymin,ymax),col="grey",xlab="",ylab="",xaxt="n",yaxt="n",cex.lab=2, cex.main=2)
+  if(controls[["sim"]]){
+    xmin = 1
+    xmax = ifelse(controls[["model"]]=="HMM",length(data$observations),dim(data$observations)[1]*(dim(data$observations)[2]-1))
+  }
+  if(!controls[["sim"]]){
+    xmin = as.Date(head(data$dates,n=1))
+    xmax = as.Date(tail(data$dates,n=1)) 
+  }
+  ymin = floor(min(data$closes)/1000)*1000 - (ceiling(max(data$closes)/1000)*1000-floor(min(data$closes)/1000)*1000)/1.5
+  ymax = max(data$closes)
+  plot(data$dates,data$closes,type="l",xlim=c(xmin,xmax),ylim=c(ymin,ymax),col="grey",xlab="",ylab="",xaxt="n",yaxt="n",cex.lab=2, cex.main=2)
   par(las=3)
-  mtext("closing price", side=4, line=4, at=mean(close), cex=1.5)
+  mtext("closing price", side=4, line=4, at=mean(data$closes), cex=1.5)
   par(las=1)
   mtext("year",side=1,line=3,cex=1.5)
-  markdates = seq(xmin,xmax,by="year")
-  markdates = markdates[1:length(markdates)%%2==1]
+  markdates = seq(xmin,xmax,by="year"); markdates = markdates[1:length(markdates)%%2==1]
   axis(1, markdates, format(markdates, "%Y"))
-  axis(4, seq(floor(min(close)/1000)*1000,ceiling(max(close)/1000)*1000,length.out=5))
-  legend("topleft",bty="n",pch=19,legend=c("coarse-scale state 1, fine-scale state 1",
-                                           "coarse-scale state 1, fine-scale state 2",
-                                           "coarse-scale state 2, fine-scale state 1",
-                                           "coarse-scale state 2, fine-scale state 2",
-                                           "coarse-scale state 3, fine-scale state 1",
-                                           "coarse-scale state 3, fine-scale state 2"),
-         col=as.vector(t(colours)),cex=1.25)
-  for(st in ordering){
-    points(date[cs_s==st&fs_s==which.max(pars$mus_star[[st]])],close[cs_s==st&fs_s==which.max(pars$mus_star[[st]])],col=colours[which(ordering==st),1],pch=20)
-    points(date[cs_s==st&fs_s==which.min(pars$mus_star[[st]])],close[cs_s==st&fs_s==which.min(pars$mus_star[[st]])],col=colours[which(ordering==st),2],pch=20)
+  axis(4, seq(floor(min(data$closes)/1000)*1000,ceiling(max(data$closes)/1000)*1000,length.out=5))
+  if(controls[["model"]]=="HMM"){
+    for(s in seq_len(states[1])){
+      points(data$dates[decoding==s],data$closes[decoding==s],col=colors[s,1],pch=20)
+    }
+    do.call(legend,c(list(legend=paste("state",seq_len(states[1])),col=colors[,1],pch=19),legend_layout))
   }
-  text(as.Date("2008-9-15"),max(close)*0.66,"bankruptcy of Lehman Brothers \nSeptember 15, 2008",pos=4,cex=1.5)
-  abline(v=as.Date("2008-09-15"),lty=2,lwd=2)
-  
+  if(controls[["model"]]=="HHMM"){
+    for(cs in seq_len(states[1])){
+      for(fs in seq_len(states[2])){
+        points(data$dates[decoding_cs==cs&decoding_fs==fs],data$closes[decoding_cs==cs&decoding_fs==fs],col=colors[cs,fs+1],pch=20)
+      }
+    }
+    eg = expand.grid(seq_len(states[1]),seq_len(states[2]))
+    do.call(legend,c(list(legend=paste0("coarse-scale state ",eg[,1],", fine-scale state ",eg[,2]),col=colors[,-1],pch=19),legend_layout))
+  }
+  if(!controls[["sim"]]){
+    for(l in seq_len(length(labels[["dates"]]))){
+      text(as.Date(labels[["dates"]][l]),max(data$closes),labels[["names"]][l],pos=4,cex=1.5)
+      abline(v=as.Date(labels[["dates"]][l]),lty=2,lwd=2)
+    }
+  }
   par(new=TRUE)
   par(mfrow=c(1,1),las=1)
-  xmin = as.Date("2000-1-1")
-  xmax = as.Date("2020-1-1")
-  ymin = min(fs_obs)
-  ymax = max(fs_obs) + 0.2
-  plot(date,fs_obs,type="h",col="grey",xlab="",ylab="",xaxt="n",yaxt="n",xlim=c(xmin,xmax),ylim=c(ymin,ymax))
+  if(controls[["model"]]=="HMM"){
+    ymin = min(data$observations); ymax = max(data$observations)
+    plot(data$dates,data$observations,type="h",col="grey",xlab="",ylab="",xaxt="n",yaxt="n",xlim=c(xmin,xmax),ylim=c(ymin,ymax+0.2))
+  }
+  if(controls[["model"]]=="HHMM"){
+    ymin = min(data$observations[,-1]); ymax = max(data$observations[,-1])
+    plot(data$dates,data$observations[,-1],type="h",col="grey",xlab="",ylab="",xaxt="n",yaxt="n",xlim=c(xmin,xmax),ylim=c(ymin,ymax+0.2))
+  }
   par(las=3)
   mtext("log-return", side=2, line=4, at=0, cex=1.5)
   par(las=1)
-  markdates = seq(xmin,xmax,by="year")
-  markdates = markdates[1:length(markdates)%%2==1]
-  axis(2, seq(-0.08,0.08,by=0.04))
-  for(st in ordering){
-    points(date[cs_s==st&fs_s==which.max(pars$mus_star[[st]])],fs_obs[cs_s==st&fs_s==which.max(pars$mus_star[[st]])],col=colours[which(ordering==st),1],pch=20)
-    points(date[cs_s==st&fs_s==which.min(pars$mus_star[[st]])],fs_obs[cs_s==st&fs_s==which.min(pars$mus_star[[st]])],col=colours[which(ordering==st),2],pch=20)
+  axis(2, round(seq(ymin,ymax,by=0.04),2))
+  if(controls[["model"]]=="HMM"){
+    for(s in seq_len(states[1])){
+      points(data$dates[decoding==s],data$observations[decoding==s],col=colors[s,1],pch=20)
+    }
+  }
+  if(controls[["model"]]=="HHMM"){
+    for(cs in seq_len(states[1])){
+      for(fs in seq_len(states[2])){
+        points(data$dates[decoding_cs==cs&decoding_fs==fs],as.vector(t(data$observations[,-1]))[decoding_cs==cs&decoding_fs==fs],col=colors[cs,fs+1],pch=20)
+      }
+    }
   }
   invisible(dev.off())
-  }
   
   ### pseudo-residuals
   if(controls[["model"]]=="HMM"){
-    T = length(data$observations)
     pseudos = numeric(T)
     for(t in 1:T){
       pseudos[t] = qnorm(pt((data$observations[t]-mus[decoding[t]])/sigmas[decoding[t]],dfs[decoding[t]]))
@@ -129,15 +166,11 @@ visual = function(data,fit,decoding,controls,labels=NULL){
     invisible(dev.off())
   }
   if(controls[["model"]]=="HHMM"){
-    T = dim(data$observations)[1]
-    T_star = dim(data$observations)[2]-1
     pseudos_cs = numeric(T)
     pseudos_fs = numeric(T*T_star)
     for(t in 1:T){
       pseudos_cs[t] = qnorm(pt((data$observations[t,1]-mus[decoding[t,1]])/sigmas[decoding[t,1]],dfs[decoding[t,1]]))
     }
-    decoding_cs = rep(decoding[,1],T_star)
-    decoding_fs = as.vector(t(decoding[,-1]))
     for(t in 1:(T*T_star)){
       pseudos_fs[t] = qnorm(pt((as.vector(t(data$observations[,-1]))[t]-mus_star[[decoding_cs[t]]][decoding_fs[t]])/sigmas_star[[decoding_cs[t]]][decoding_fs[t]],dfs_star[[decoding_cs[t]]][decoding_fs[t]]))
     }
