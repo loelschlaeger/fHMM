08cf116fd4fe41c07bdf882cedbebf828821cda0
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-01 19:14:49 +0100

    new label

--- main.R
+++ main.R
@@ -1,16 +1,16 @@
-### 1. Initialization
 rm(list = ls())
+
+### 1. Initialization
 source("init.R"); init()
 
 ### 2. Set and check controls
 controls = list(
-  model_name    = "test",        
-  data_source   = c("dax.csv","deutschebank.csv"),
-  truncate_data = c("2000-01-01","2021-01-01"), 
-  states        = c(3,2),
+  model_name    = "HMM_3_DAX",        
+  data_source   = c("dax.csv",NA),
+  truncate_data = c("2000-01-03",NA), 
+  states        = c(3,0),
   time_horizon  = c(NA,30),
-  runs          = 200,
-  overwrite     = FALSE
+  runs          = 200
 )
 controls = check_controls(controls)
 
@@ -23,10 +23,10 @@ decoding = applyViterbi(data,fit,controls)
 
 ### 5. Visualize results
 labels = list(
-  dates = c("2008-09-15","2020-01-27"),
-  names = c("Bankruptcy of Lehman Brothers","First COVID-19 case in Germany")
+  dates = c("2001-09-11","2008-09-15","2020-01-27"),
+  names = c("9/11 terrorist attack","Bankruptcy of Lehman Brothers","First COVID-19 case in Germany")
 )
 visual(data,fit,decoding,controls,labels)
 
 ### 6. Reinitialize model
-reinit("test")
+reinit("HMM_DAX_3")
