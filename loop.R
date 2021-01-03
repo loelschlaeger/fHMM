10c302e020f3e43ef614202d23c0fab816479cb1
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-03 16:57:18 +0100

    removed models

--- loop.R
+++ loop.R
@@ -8,42 +8,28 @@ options(warn=1)
 
 ### 3. Set controls for different models
 all_controls = list(
+  # list(
+  #   id            = "HHMM_3_2_sim",
+  #   time_horizon  = c(300,30),
+  #   states        = c(3,2)
+  #   ),
   list(
-    model_name    = "HMM_3_sim",        
-    time_horizon  = c(1000,NA),
-    states        = c(3,0)
-  ),
-  list(
-    model_name    = "HHMM_3_2_sim",        
-    time_horizon  = c(3000,30),
-    states        = c(3,2)
-  ),
-  list(
-    model_name    = "HMM_2_SandP500",        
-    data_source   = c("sandp500.csv",NA),
-    truncate_data = c("2000-01-03",NA), 
-    states        = c(2,0)
-  ),
-  list(
-    model_name    = "HMM_3_SandP500",        
-    data_source   = c("sandp500.csv",NA),
-    truncate_data = c("2000-01-03",NA), 
-    states        = c(3,0)
-  ),
-  list(
-    model_name    = "HMM_4_SandP500",        
-    data_source   = c("sandp500.csv",NA),
-    truncate_data = c("2000-01-03",NA), 
-    states        = c(4,0)
-  ),
-  list(
-    model_name    = "HHMM_2_2_DAX",        
-    data_source   = c(NA,"dax.csv"),
-    truncate_data = c("2000-01-03",NA), 
+    id            = "HHMM_2_2_DAX_DB",
+    data_source   = c("dax.csv","deutschebank.csv"),
+    data_col      = c("Close","Close"),
+    truncate_data = c("2000-01-03",NA),
     states        = c(2,2),
     time_horizon  = c(NA,30)
+    )
+  # list(
+  #   id            = "HHMM_2_2_SandP500",
+  #   data_source   = c(NA,"sandp500.csv"),
+  #   data_col      = c(NA,"Close"),
+  #   truncate_data = c("2000-01-03",NA),
+  #   states        = c(2,2),
+  #   time_horizon  = c(NA,30)
+  #   )
   )
-)
 
 ### 4. Pre-check controls
 for(control_no in seq_len(length(all_controls))){
@@ -61,7 +47,7 @@ for(controls in all_controls){
     labels = list(
       dates = c("2001-09-11","2008-09-15","2020-01-27"),
       names = c("9/11 terrorist attack","Bankruptcy of Lehman Brothers","First COVID-19 case in Germany")
-    )
+      )
     visual(data,fit,decoding,controls,labels)
   }, error=function(e){cat("Estimation failed:",conditionMessage(e),"\n")})
 }
