439686a155570724fc9cafcfb5b05fab8d557b43
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-03 16:56:48 +0100

    renamed "observations" and "closes"

--- data.R
+++ data.R
@@ -1,15 +1,9 @@
 getData = function(controls){
   if(is.null(controls[["controls_checked"]])) stop("'controls' invalid",call.=FALSE)
-  if(controls[["sim"]]){
-    data = simulateData(controls)
-    if(any(dim(t(data[["observations"]]))==0)) stop("Too few data points.",call.=FALSE)
-    writeLines("Data simulation successful.")
-  }
-  if(!controls[["sim"]]){
-    data = readData(controls)
-    if(any(dim(t(data[["observations"]]))==0)) stop("Too few data points.",call.=FALSE)
-    writeLines("Data processing successful.")
-  }
+  if(controls[["sim"]]) data = simulateData(controls)
+  if(!controls[["sim"]]) data = readData(controls)
+  if(any(dim(t(data[["logReturns"]]))==0)) stop("Too few data points.",call.=FALSE)
+  writeLines("Data processing successful.")
   check_data(controls,data)
   return(data)
 }
@@ -17,12 +11,8 @@ getData = function(controls){
 ### simulate data
 simulateData = function(controls){
   if(!is.null(controls[["seed"]])) set.seed(controls[["seed"]])
-  M = controls[["states"]][1] #coarse-scale states
-  N = controls[["states"]][2] #fine-scale states
-  if(!controls[["est_dfs"]]){
-    df_cs = controls[["fix_dfs"]][1]
-    df_fs = controls[["fix_dfs"]][2]
-  }
+  M = controls[["states"]][1] #HMM states / HHMM coarse-scale states
+  N = controls[["states"]][2] #HHMM fine-scale states
   T = controls[["time_horizon"]][1]
   T_star = controls[["time_horizon"]][2]
   
@@ -31,78 +21,100 @@ simulateData = function(controls){
   thetaList = thetaCon2thetaList(thetaCon,controls)
   
   simulateStates = function(delta,Gamma,T){
-    N = length(delta)
+    no_states = length(delta)
     states = numeric(T)
-    states[1] = sample(1:N,1,prob=delta)
+    states[1] = sample(1:no_states,1,prob=delta)
     for(t in 2:T){
-      states[t] = sample(1:N,1,prob=Gamma[states[t-1],])
+      states[t] = sample(1:no_states,1,prob=Gamma[states[t-1],])
     }
     return(states)
   }
   
-  simulateObservations = function(states,mus,sigmas,dfs,T){
-    obs = numeric(T)
+  simulateLogReturns = function(states,mus,sigmas,dfs){
+    T = length(states)
+    logReturns = numeric(T)
     for(t in 1:T){
-      obs[t] = rt(1,dfs[states[t]])*sigmas[states[t]]+mus[states[t]]
+      logReturns[t] = rt(1,dfs[states[t]])*sigmas[states[t]]+mus[states[t]]
     }
-    return(obs)
+    return(logReturns)
   }
   
-  if(controls$model=="HMM"){ 
+  if(controls[["model"]]=="HMM"){ 
     states = simulateStates(Gamma2delta(thetaList[["Gamma"]]),thetaList[["Gamma"]],T) 
-    observations = simulateObservations(states,thetaList[["mus"]],thetaList[["sigmas"]],thetaList[["dfs"]],T)
+    logReturns = simulateLogReturns(states,thetaList[["mus"]],thetaList[["sigmas"]],thetaList[["dfs"]])
   }
   
-  if(controls$model=="HHMM"){ 
+  if(controls[["model"]]=="HHMM"){ 
     states = matrix(0,T,T_star+1) 
-    observations = matrix(0,T,T_star+1)
+    logReturns = matrix(0,T,T_star+1)
     states[,1] = simulateStates(Gamma2delta(thetaList[["Gamma"]]),thetaList[["Gamma"]],T) 
-    observations[,1] = simulateObservations(states[,1],thetaList[["mus"]],thetaList[["sigmas"]],thetaList[["dfs"]],T)
+    logReturns[,1] = simulateLogReturns(states[,1],thetaList[["mus"]],thetaList[["sigmas"]],thetaList[["dfs"]])
     for(t in 1:T){
       S_t = states[t,1]
       states[t,-1] = simulateStates(Gamma2delta(thetaList[["Gammas_star"]][[S_t]]),thetaList[["Gammas_star"]][[S_t]],T_star)
-      observations[t,-1] = simulateObservations(states[t,-1],thetaList[["mus_star"]][[S_t]],thetaList[["sigmas_star"]][[S_t]],thetaList[["dfs_star"]][[S_t]],T_star)
+      logReturns[t,-1] = simulateLogReturns(states[t,-1],thetaList[["mus_star"]][[S_t]],thetaList[["sigmas_star"]][[S_t]],thetaList[["dfs_star"]][[S_t]])
     }
   }
   
   out = list(
-    "observations" = observations,
+    "logReturns"   = logReturns,
     "states0"      = states,
     "thetaUncon0"  = thetaUncon,
     "thetaCon0"    = thetaCon,
     "thetaList0"   = thetaList
-  )
+    )
   
   return(out)
 }
 
 ### read financial data from csv file
 readData = function(controls){
-  dataSource = controls[["data_source"]]
+  data_source = controls[["data_source"]]
+  data_col = controls[["data_col"]]
+  
   data = list()
   for(i in 1:2){
-    if(!is.na(dataSource)[i]){
-      data[[i]] = read.csv(file=paste0("data/",dataSource[i]),head=TRUE,sep=",",na.strings="null")
-      if(!all(c("Date","Close") %in% colnames(data[[i]]))) stop("Data file must have two columns named 'Date' and 'Close'.")
-      data[[i]] = data[[i]][,colnames(data[[i]]) %in% c("Date","Close"), drop = F]
+    if(is.na(data_source)[i]){
+      data[[i]] = NA
+    }
+    
+    if(!is.na(data_source)[i]){
+      
+      ### extract data
+      data[[i]] = read.csv(file=paste0("data/",data_source[i]),head=TRUE,sep=",",na.strings="null")
+      if(!"Date" %in% colnames(data[[i]])) stop(paste0("Data file '",data_source[i],"' must have a column named 'Date'."))
+      if(!data_col[i] %in% colnames(data[[i]])) stop(paste0("Data file '",data_source[i],"' does not have a column named '",data_col[i],"'."))
+      data[[i]] = data[[i]][,colnames(data[[i]]) %in% c("Date",data_col[i]), drop = FALSE]
       data[[i]][["Date"]] = as.Date(data[[i]][["Date"]], format="%Y-%m-%d")
-      for(na_close in which(is.na(data[[i]][["Close"]]))){ ### replace NA-close values of neighbour mean
+      data[[i]][[data_col[i]]] = as.numeric(data[[i]][[data_col[i]]])
+      
+      ### remove NA dates
+      data[[i]] = data[[i]][!is.na(data[[i]][["Date"]]),]
+      
+      ### replace NA values by neighbour means
+      for(na_value in which(is.na(data[[i]][[data_col[i]]]))){
         incr = 1
         while(TRUE){
-          range = unique(abs(c((na_close-incr):(na_close-1),(na_close+1):(na_close+incr))))
-          replace = mean(data[[i]][["Close"]][range],na.rm=TRUE)
-          if(!is.na(replace)) data[[i]][["Close"]][na_close] = replace; break
+          range = unique(abs(c((na_value-incr):(na_value-1),(na_value+1):(na_value+incr))))
+          replace = mean(data[[i]][[data_col[i]]][range],na.rm=TRUE)
+          if(!is.nan(replace)){
+            data[[i]][[data_col[i]]][na_value] = replace
+            break
+          }
           incr = incr + 1
         }
       }
-      data[[i]][["LogReturns"]] = numeric(length(data[[i]][["Close"]]))
-      for(t in seq_len(length(data[[i]][["Close"]]))[-1]){
-        data[[i]][["LogReturns"]][t] = log(data[[i]][["Close"]][t]/data[[i]][["Close"]][t-1])
+      
+      ### compute log-returns
+      data_length = length(data[[i]][[data_col[i]]])
+      data[[i]][["LogReturns"]] = numeric(data_length)
+      for(t in seq_len(data_length)[-1]){
+        data[[i]][["LogReturns"]][t] = log(data[[i]][[data_col[i]]][t]/data[[i]][[data_col[i]]][t-1])
       }
     }
   }
   
-  ### truncate 'data' based on 'controls[["trunc_data"]]'
+  ### truncate the data based on 'controls[["trunc_data"]]'
   truncateData = function(controls,data){
     ### find exact or nearest position of 'date' in 'data' 
     findDate = function(date,data){
@@ -132,60 +144,40 @@ readData = function(controls){
     data[[1]] = truncateData(controls,data[[1]])
     
     out = list(
-      "observations" = data[[1]][["LogReturns"]],
-      "closes" = data[[1]][["Close"]],
+      "logReturns" = data[[1]][["LogReturns"]],
+      "dataRaw" = data[[1]][[data_col[1]]],
       "dates" = data[[1]][["Date"]]
-    )
+      )
   }
   
   ### HHMM data
   if(controls[["model"]]=="HHMM"){
+      
+    ### remove data points that do not occur in both files
+    data[[1]] = data[[1]][ data[[1]][["Date"]] %in% intersect(data[[1]][["Date"]],data[[2]][["Date"]]), ]
+    data[[2]] = data[[2]][ data[[2]][["Date"]] %in% intersect(data[[2]][["Date"]],data[[1]][["Date"]]), ]
     
-    ### same data on fine and coarse scale
-    if(controls[["HHMM_av"]]){
-      data[[2]] = truncateData(controls,data[[2]])
-      
-      T_star = controls[["time_horizon"]][2]
-      T = floor(length(data[[2]]$Close)/T_star)
-      data[[2]] = data[[2]][1:(T*T_star),]
-      fs_obs = matrix(data[[2]][["LogReturns"]],ncol=T_star,nrow=T,byrow=TRUE)
-      cs_obs = numeric(T)
-      for(t in 1:T){
-        cs_obs[t] = mean(fs_obs[t,])
-      }
-      
-      out = list(
-        "observations" = cbind(cs_obs,fs_obs,deparse.level=0),
-        "closes" = data[[2]][["Close"]],
-        "dates" = data[[2]][["Date"]]
+    data[[1]] = truncateData(controls,data[[1]])
+    data[[2]] = truncateData(controls,data[[2]])
+    
+    if(any(dim(data[[1]])!=dim(data[[2]]))) stop("Processing of the datasets failed.")
+    data_length = length(data[[1]][["Date"]])
+    
+    T_star = controls[["time_horizon"]][2]
+    T = floor(data_length/T_star)
+    data[[1]] = data[[1]][1:(T*T_star),]
+    data[[2]] = data[[2]][1:(T*T_star),]
+    
+    cs_logReturns = rowMeans(matrix(data[[1]][["LogReturns"]],ncol=T_star,nrow=T,byrow=TRUE))
+    fs_logReturns = matrix(data[[2]][["LogReturns"]],ncol=T_star,nrow=T,byrow=TRUE)
+    logReturns = cbind(cs_logReturns,fs_logReturns,deparse.level=0)
+    
+    out = list(
+      "logReturns" = logReturns,
+      "dataRaw"    = data[[2]][[data_col[2]]],
+      "dataRaw_cs" = data[[1]][[data_col[1]]],
+      "dates"      = data[[2]][["Date"]]
       )
-    }
-      
-    ### different data on fine and coarse scale
-    if(!controls[["HHMM_av"]]){
-      ### remove data points that do not occur in both files
-      data[[1]] = data[[1]][data[[1]][["Date"]] %in% intersect(data[[1]][["Date"]],data[[2]][["Date"]]),]
-      data[[2]] = data[[2]][data[[2]][["Date"]] %in% intersect(data[[2]][["Date"]],data[[1]][["Date"]]),]
-      
-      data[[1]] = truncateData(controls,data[[1]])
-      data[[2]] = truncateData(controls,data[[2]])
-      
-      if(any(dim(data[[1]])!=dim(data[[2]]))) stop()
-      
-      T_star = controls[["time_horizon"]][2]
-      T = floor(length(data[[1]]$Close)/T_star)
-      data[[1]] = data[[1]][1:(T*T_star),]
-      data[[2]] = data[[2]][1:(T*T_star),]
-      fs_obs = matrix(data[[2]][["LogReturns"]],ncol=T_star,nrow=T,byrow=TRUE)
-      cs_obs = rowMeans(matrix(data[[1]][["LogReturns"]],ncol=T_star,nrow=T,byrow=TRUE))
-      
-      out = list(
-        "observations" = cbind(cs_obs,fs_obs,deparse.level=0),
-        "closes" = data[[2]][["Close"]],
-        "closes_cs" = data[[1]][["Close"]],
-        "dates" = data[[2]][["Date"]]
-      )
-    }
   }
 	
 	return(out)
