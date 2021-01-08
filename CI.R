3acf467775edf192d9ad5e0aef0f92e57a18ad60
Timo Adam <timo.adam@uni-bielefeld.de>
2021-01-08 11:32:30 +0000

    Update CI.R.
    Fix bug.

--- CI.R
+++ CI.R
@@ -3,7 +3,7 @@
 ##  fit: a fitted model
 ##  alpha: confidence level, default 0.95
 ## Output: a list with elements lower_limit, estimate, and upper limit
-conf_int = function (fit, alpha = 0.95) {
+conf_int <- function (fit, alpha = 0.95) {
   # Input checks
   if (is.null(fit$hessian)) {
     stop("Error: no Hessian matrix supplied. Set hessian = TRUE in controls.")
@@ -15,8 +15,8 @@ conf_int = function (fit, alpha = 0.95) {
   # Hessian checks (to do)
   inv_fisher = ginv(fisher)
   sds = sqrt(diag(inv_fisher))
-  lower_limit = fit$estimates + qnorm(p = (1 - alpha) / 2) * sds
-  upper_limit = fit$estimates + qnorm(p = alpha / 2) * sds
-  out = list(lower_limit = lower_limit, estimate = fit$estimate, upper_limit = upper_limit)
-  return(out)
+  lower_limit = fit$estimate + qnorm(p = (1 - alpha) / 2) * sds
+  upper_limit = fit$estimate + qnorm(p = 1 - (1 - alpha) / 2) * sds
+  df = data.frame(lower_limit = lower_limit, estimate = fit$estimate, upper_limit = upper_limit)
+  return(df)
 }
