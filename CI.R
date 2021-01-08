0550ab3508b5d19eb794b93e7264400dd4a5dae0
Timo Adam <timo.adam@uni-bielefeld.de>
2021-01-08 11:24:54 +0000

    Update CI.R.
    New input checks.

--- CI.R
+++ CI.R
@@ -1,15 +1,18 @@
 ## Compute confidence intervals for the estimates
 ## Inputs:
-##  fit: fitted model
-##  alpha: confidence level
+##  fit: a fitted model
+##  alpha: confidence level, default 0.95
 ## Output: a list with elements lower_limit, estimate, and upper limit
-
 conf_int = function (fit, alpha = 0.95) {
+  # Input checks
   if (is.null(fit$hessian)) {
-    stop("Error: no Hessian matrix supplied.")
+    stop("Error: no Hessian matrix supplied. Set hessian = TRUE in controls.")
+  }
+  if (alpha < 0 | alpha > 1) {
+    stop("Error: alpha needs to be between 0 and 1.")
   }
   fisher = fit$hessian
-  ## Hessian checks (to do)
+  # Hessian checks (to do)
   inv_fisher = ginv(fisher)
   sds = sqrt(diag(inv_fisher))
   lower_limit = fit$estimates + qnorm(p = (1 - alpha)) * sds
