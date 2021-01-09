f1982dbf2975d09b7457515356d700810d2bf50a
Timo Adam <timo.adam@uni-bielefeld.de>
2021-01-09 15:36:52 +0000

    Update documentation.

--- CI.R
+++ CI.R
@@ -1,7 +1,7 @@
 #' Compute confidence intervals for the estimates
 #'
 #' @param fit A fitted model
-#' @param alpha confidence level, default 0.95
+#' @param alpha Confidence level, default 0.95
 #'
 #' @return A list containing the following elements:
 #' \item{lower_limit}{the lower limits of the intervals}
