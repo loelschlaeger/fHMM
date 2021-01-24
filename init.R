dc34c1f1331f58b6cff98232996994dfd17c8a4d
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-24 02:10:16 +0100

    main function 'hhmmf'

--- init.R
+++ init.R
@@ -22,47 +22,46 @@ load_code = function(){
               source("viterbi.R"))
   for(e in seq_len(length(exe))){
     suppressPackageStartupMessages(eval(exe[[e]]))
-    cat(sprintf("Loading HHMM_Finance code: %.0f%%",(e/length(exe)*100)),"\r")
+    message(sprintf("Loading HHMM_Finance code: %.0f%%",(e/length(exe)*100)),"\r",appendLF=FALSE)
   }
-  cat("\n")
-  writeLines(paste0("Data source:   ",getwd(),"/data"))
-  writeLines(paste0("Model results: ",getwd(),"/models"))
+  message(paste0("Data source: ",getwd(),"/data"))
+  message(paste0("Model results: ",getwd(),"/models"))
 }
 
-### initialize the estimation routine randomly
-init_est = function(controls){
-  M  = controls[["states"]][1] 
-  N  = controls[["states"]][2] 
-  df_cs = controls[["fixed_dfs"]][1]
-  df_fs = controls[["fixed_dfs"]][2]
+### define main function
+hhmmf = function(id="test",controls,events=NULL,warn=0,simpar=NULL){
+  ### save 'id' in 'controls'
+  controls[["id"]] = id
   
-  gammasUncon = gammasCon2gammasUncon(runif((M-1)*M,0,1/M),M)
-  mus         = sort(rnorm(M)*10^(-1),decreasing=TRUE) 
-  sigmasUncon = log(sort(runif(M,0.1,1)*10^(-2),decreasing=FALSE))
-  dfs         = if(is.na(df_cs)) runif(M,0,30) else integer(0)
-  if(controls[["model"]]=="HHMM") for(m in seq_len(M)){
-    gammasUncon = c(gammasUncon,gammasCon2gammasUncon(runif((N-1)*N,0,1/N),N))
-    mus         = c(mus,sort(rnorm(N)*10^(-1),decreasing=TRUE))
-    sigmasUncon = c(sigmasUncon,log(sort(runif(N,0.1,1)*10^(-2),decreasing=FALSE)))
-    dfs         = c(dfs,if(is.na(df_fs)) runif(N,0,30) else integer(0))
+  ### set handling of warnings
+  options(warn=warn); on.exit(options(warn=0))
+  
+  ### create output folder
+  if(dir.exists(paste0("models/",id)) & id!="test"){
+    stop(paste0("Model '",id,"' already exists."),call.=FALSE)
+  } else {
+    if(!dir.exists(paste0("models/",id))){
+      dir.create(paste0("models/",id))
+    }
   }
   
-  thetaUncon = c(gammasUncon,mus,sigmasUncon,dfs)
-  return(thetaUncon)
-}
-
-### load .rds-files of the old model 'id'
-load_model = function(id){
-  path = paste0("models/",id)
-  if(!dir.exists(path)) stop(paste0("Path '",path,"' does not exist."),call.=FALSE)
-  loadable = list.files(path=path,pattern="*.rds")
-  loaded = list()
-  for(object in loadable) loaded[[object]] = readRDS(paste0(path,"/",object))
-  if(length(loaded)>=1){
-    writeLines("Reinitialization successful.")
-    writeLines(paste0("ID:      ",id))
-    writeLines(paste0("Objects: ",paste0(names(loaded),collapse=", ")))
-  }
-  if(length(loaded)==0) stop(paste0("Unable to load any object from path '",path,"'."),call.=FALSE)
-  invisible(list2env(loaded, envir = .GlobalEnv))
+  ### execute model fitting 
+  seperator = paste0(rep("-",45),collapse="")
+  tryCatch(
+    {sink(file = paste0("models/",id,"/protocol.txt"),split = TRUE)
+       controls = check_controls(controls); writeLines(seperator)
+       data     = get_data(controls,simpar); writeLines(seperator)
+       fit      = max_likelihood(data,controls)
+       decoding = apply_viterbi(data,fit,controls)
+     sink()
+     create_visuals(data,fit,decoding,controls,events)
+    },
+    error = function(cond) message(cond),
+    finally = {
+      ### close all sink connections
+      for(i in seq_len(sink.number())){
+        sink()
+      }
+    }
+  )
 }
