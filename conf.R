e65c3618d57fadb5a32231e1861d5f3328287864
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-25 21:08:18 +0100

    minor

--- conf.R
+++ conf.R
@@ -10,13 +10,10 @@
 #'
 #' @examples
 #' conf_int(fit = fit)
-conf_int <- function (fit, alpha = 0.95) {
+conf_int = function (fit, alpha = 0.95) {
   # Input checks
-  if (is.null(fit$hessian)) {
-    stop("No Hessian matrix supplied. Set 'hessian = TRUE' in 'controls'.")
-  }
   if (alpha < 0 | alpha > 1) {
-    stop("Value of 'alpha' needs to be between 0 and 1.")
+    stop("Value of 'alpha' needs to be between 0 and 1.",call.=FALSE)
   }
   fisher = fit$hessian
   # Hessian checks (to do)
@@ -26,7 +23,7 @@ conf_int <- function (fit, alpha = 0.95) {
   upper_limit = fit$estimate + qnorm(p = 1 - (1 - alpha) / 2) * sds
   out = list(lower_limit = lower_limit, estimate = fit$estimate, upper_limit = upper_limit)
   # Output checks
-  if (any(is.na(out))) {
+  if(any(is.na(out))) {
     warning("Some confidence intervals could not be computed. The corresponding estimates may lie close to the boundaries of their parameter space.")
   }
   return(out)
