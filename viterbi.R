944c6ce0747c2905155904fa3418de7ef78eaec3
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-27 19:27:37 +0100

    viterbi adapted to gamma sdd

--- viterbi.R
+++ viterbi.R
@@ -1,11 +1,16 @@
 ### definition of Viterbi algorithm for state decoding
-viterbi = function(observations,nstates,Gamma,mus,sigmas,dfs){
+viterbi = function(observations,nstates,Gamma,mus,sigmas,dfs,sdd){
 		T = length(observations)
 		delta = Gamma2delta(Gamma)
 		allprobs = matrix(0,nstates,T)
 		
 		for(n in seq_len(nstates)){
-			allprobs[n,] = (1/sigmas[n])*dt((observations-mus[n])/sigmas[n],dfs[n])
+			if(sdd=="t"){
+			  allprobs[n,] = (1/sigmas[n])*dt((observations-mus[n])/sigmas[n],dfs[n])
+			}
+		  if(sdd=="gamma"){
+		    allprobs[n,] = dgamma(observations,shape=mus[n]^2/sigmas[n]^2,scale=sigmas[n]^2/mus[n])
+		  }
 		}
 		
 		xi = matrix(0,nstates,T)
@@ -36,17 +41,35 @@ apply_viterbi = function(data,fit,controls){
   states = controls[["states"]]
   
   if(controls[["model"]]=="HMM"){
-    decoding = viterbi(observations,states[1],thetaList[["Gamma"]],thetaList[["mus"]],thetaList[["sigmas"]],thetaList[["dfs"]])
+    decoding = viterbi(observations,
+                       states[1],
+                       thetaList[["Gamma"]],
+                       thetaList[["mus"]],
+                       thetaList[["sigmas"]],
+                       thetaList[["dfs"]],
+                       controls[["sdds"]][1])
   }
   
   if(controls[["model"]]=="HHMM"){
     T = dim(observations)[1]
     T_star = data[["T_star"]]
     decoding = matrix(NA,ncol=max(T_star)+1,nrow=T)
-    decoding[,1] = viterbi(observations[,1],states[1],thetaList[["Gamma"]],thetaList[["mus"]],thetaList[["sigmas"]],thetaList[["dfs"]])
+    decoding[,1] = viterbi(observations[,1],
+                           states[1],
+                           thetaList[["Gamma"]],
+                           thetaList[["mus"]],
+                           thetaList[["sigmas"]],
+                           thetaList[["dfs"]],
+                           controls[["sdds"]][1])
     for(t in seq_len(T)){
       curr = decoding[t,1]
-      decoding[t,-1] = c(viterbi(observations[t,-1][!is.na(observations[t,-1])],states[2],thetaList[["Gammas_star"]][[curr]],thetaList[["mus_star"]][[curr]],thetaList[["sigmas_star"]][[curr]],thetaList[["dfs_star"]][[curr]]),rep(NA,max(T_star)-T_star[t]))
+      decoding[t,-1] = c(viterbi(observations[t,-1][!is.na(observations[t,-1])],
+                                 states[2],
+                                 thetaList[["Gammas_star"]][[curr]],
+                                 thetaList[["mus_star"]][[curr]],
+                                 thetaList[["sigmas_star"]][[curr]],
+                                 thetaList[["dfs_star"]][[curr]],
+                                 controls[["sdds"]][2]),rep(NA,max(T_star)-T_star[t]))
     }
   }
   
