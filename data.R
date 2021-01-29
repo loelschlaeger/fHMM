3470ab6adc21423517eb4052d6850188cd6e3a45
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-29 20:29:28 +0100

    remove 0 log-returns in case of gamma sdd

--- data.R
+++ data.R
@@ -173,6 +173,21 @@ read_data = function(controls){
       for(t in seq_len(data_length)[-1]){
         data[[i]][["LogReturns"]][t] = log(data[[i]][[data_col[i]]][t]/data[[i]][[data_col[i]]][t-1])
       }
+      
+      ### remove 0 log-returns in case of gamma sdd to avoid numerical conflicts
+      if(controls[["sdds"]][i]=="gamma"){
+        for(t in seq_len(data_length)){
+          if(data[[i]][["LogReturns"]][t]==0){
+            step = 1
+            cand = 0
+            while(cand==0){
+              cand = mean(data[[i]][["LogReturns"]][abs((t-step):(t+step))],na.rm=TRUE)
+              step = step + 1
+            }
+            data[[i]][["LogReturns"]][t] = cand
+          }
+        }
+      }
     }
   }
   
@@ -241,10 +256,13 @@ read_data = function(controls){
     
     ### transform CS data
     if(controls[["data_cs_type"]] == "mean"){
-      cs_data = rowMeans(cs_data_tbt,na.rm = TRUE)
+      cs_data = rowMeans(cs_data_tbt,na.rm=TRUE)
+    }
+    if(controls[["data_cs_type"]] == "mean_abs"){
+      cs_data = rowMeans(abs(cs_data_tbt),na.rm=TRUE)
     }
     if(controls[["data_cs_type"]] == "sum_abs"){
-      cs_data = rowSums(abs(cs_data_tbt),na.rm = TRUE)
+      cs_data = rowSums(abs(cs_data_tbt),na.rm=TRUE)
     }
     
     out = list(
@@ -257,81 +275,4 @@ read_data = function(controls){
   }
 	
 	return(out)
-}
-
-### download data from www.finance.yahoo.com
-download_data = function(name=NULL,symbol=NULL,from=as.Date("1902-01-01"),to=Sys.Date(),show_symbols=FALSE){
-  
-  ### load and sort or create 'stock_symbols'
-  if(file.exists("data/stock_symbols.rds")){
-    stock_symbols = readRDS("data/stock_symbols.rds")
-    stock_symbols = stock_symbols[order(stock_symbols["name"]),]
-  } else {
-    stock_symbols = data.frame("name"=character(),"symbol"=character())
-    saveRDS(stock_symbols,file="data/stock_symbols.rds")
-  }
-  
-  ### print 'stock_symbols'
-  if(show_symbols){
-    if(dim(stock_symbols)[1]!=0){
-      print(stock_symbols,row.names = FALSE)
-    } else {
-      message("No saved stock symbols.")
-    }
-  } 
-  if(!is.null(name)){  
-    ### convert 'from' and 'to' to dates
-    from = as.Date(from)
-    to = as.Date(to)
-  
-    ### define minimum date 'from'
-    min_date = as.Date("1902-01-01")
-    if(from < min_date) from = min_date
-    
-    ### function to create finance.yahoo.com-URL
-    create_url = function(symbol,from,to){
-      t1 = as.integer(ISOdate(as.numeric(format(from,format="%Y")),as.numeric(format(from,format="%m")),as.numeric(format(from,format="%d")),hour=0))
-      t2 = as.integer(ISOdate(as.numeric(format(to,format="%Y")),as.numeric(format(to,format="%m")),as.numeric(format(to,format="%d")),hour=24))
-      url = paste("https://query1.finance.yahoo.com/v7/finance/download/",symbol,"?period1=",t1,"&period2=",t2,"&interval=1d&events=history",sep="")
-      return(url)
-    }
-    
-    ### covert 'name' to lowercase
-    name = tolower(name)
-    
-    ### search 'name' in 'stock_symbols' and get corresponding 'symbol'
-    if(is.null(symbol)){
-      if(name %in% stock_symbols[["name"]]){
-        symbol = stock_symbols[which(stock_symbols["name"]==name),"symbol"]
-      } else {
-        stop(paste0("Symbol for the stock '",name,"' is unknown."),call.=FALSE)
-      }
-    } else {
-      if(name %in% stock_symbols["name"]){
-        symbol = stock_symbols[which(stock_symbols["name"]==name),"symbol"]
-      } else {
-        read_try = suppressWarnings(try(read.csv(create_url(symbol,from,to)),silent=TRUE))
-        if(inherits(read_try, "try-error")){
-          stop(paste0("Symbol '",symbol,"' is invalid."),call.=FALSE)
-        } else {
-          ### save new symbol
-          stock_symbols[nrow(stock_symbols)+1,] = c(name,symbol)
-          saveRDS(stock_symbols,file="data/stock_symbols.rds")
-        }
-      }
-    }
-    
-    ### download and save data
-    filename = paste0("data/",name,".csv")
-    download.file(create_url(symbol,from,to),destfile=filename,quiet=TRUE)
-    
-    ### print summary of new data
-    data = read.csv(file=filename,head=TRUE,sep=",",na.strings="null") 
-    message("Data download successful.")
-    message(paste("Source:",paste0(name,".csv")))
-    message(paste("Symbol:",symbol))
-    message(paste("From:",head(data$Date,n=1)))
-    message(paste("To:",tail(data$Date,n=1)))
-  }
-  
 }
