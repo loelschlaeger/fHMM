5c451f9f4da1e427f35c07bb2823e628a2d11f28
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-01-29 20:28:28 +0100

    checks for start values

--- optim.R
+++ optim.R
@@ -1,6 +1,6 @@
 ### maximize log-likelihood function
 max_likelihood = function(data,controls){
-  if(is.null(controls[["controls_checked"]])) stop("'controls' invalid",call.=FALSE)
+  if(is.null(controls[["controls_checked"]])) stop("'controls' is invalid (Code C.1)",call.=FALSE)
   if(!is.null(controls[["seed"]])) set.seed(controls[["seed"]])
   
   runs = controls[["runs"]]
@@ -10,57 +10,84 @@ max_likelihood = function(data,controls){
   ### define optimizer
   if(controls[["model"]]=="HMM") target = nLL_hmm
   if(controls[["model"]]=="HHMM") target = nLL_hhmm
-  optimized = function(start_values){
+  optimized = function(start_value){
     nlm_out = nlm(f = target,
-                  p = start_values,
+                  p = start_value,
                   observations = data[["logReturns"]],
                   controls = controls,
                   iterlim = controls[["iterlim"]],
                   steptol = controls[["steptol"]],
                   gradtol = controls[["gradtol"]],
                   print.level = controls[["print_level"]],
-                  typsize = start_values,
+                  typsize = start_value,
                   hessian = FALSE)
     return(nlm_out)
   }
   
   ### generate start values
-  start_values = list()
-  if(controls[["at_true"]]){
-    start_values[[1]] = data[["thetaUncon0"]]
-  }
-  if(!controls[["at_true"]]){
-    for(run in seq_len(runs)){
-      start_values[[run]] = init_est(controls)
+  generate_start_values = function(controls,runs){
+    start_values = list()
+    if(controls[["at_true"]]){
+      start_values[[1]] = data[["thetaUncon0"]]
     }
+    if(!controls[["at_true"]]){
+      for(run in seq_len(runs)){
+        start_values[[run]] = init_est(controls)
+      }
+    }
+    return(start_values)
   }
   
-  ### check if log-likelihoods at start values can be computed
-  no_failes = function(llks){
-    total = length(llks)
-    failes = length(llks[is.nan(llks)||is.na(llks)||abs(llks)>1e100])
-    return(failes/total)
+  ### check if log-likelihoods at start values can be computed 
+  failed_start_values = function(values){
+    failed = union(which(is.nan(values)),which(is.na(values)))
+    failed = c(failed,which(abs(values[-failed])>1e100))
+    return(failed)
   }
-  ll_at_start_values = numeric(runs)
+  
+  ### adjust 'scale_par' based on method of moments estimates
+  adjust_scale_par = function(controls,data){
+    scale_par = c(NA,NA)
+    if(controls[["model"]]=="HMM"){
+      scale_par[1] = mean(c(mean(data,na.rm="TRUE"),sd(data,na.rm="TRUE")))
+    }
+    if(controls[["model"]]=="HHMM"){
+      scale_par[1] = mean(c(mean(data[,1],na.rm="TRUE"),sd(data[,1],na.rm="TRUE")))
+      scale_par[2] = mean(c(mean(data[,-1],na.rm="TRUE"),sd(data[,-1],na.rm="TRUE")))
+    }
+    return(scale_par)
+  }
+  
+  message("Selecting start values...",appendLF = FALSE)
+  controls[["scale_par"]] = adjust_scale_par(controls,data[["logReturns"]])
+  start_values = generate_start_values(controls,runs)
+  ll_at_start_values = rep(NA,runs)
   for(run in seq_len(runs)){
     ll_at_start_values[run] = suppressWarnings(target(start_values[[run]],data[["logReturns"]],controls))
-    if(no_failes(ll_at_start_values)>0.5){
-      stop("Bad parameter scaling. (Code 10)",call.=FALSE)
-    }
+  }
+  message("\r",sprintf("Start values selected. %10s"," "))
+  fails = failed_start_values(ll_at_start_values)
+  if(length(fails)/runs>0.5){
+    stop("... (Code F.*)",call.=FALSE)
+  }
+  if(length(fails)>0){
+    runs_seq = seq_len(runs)[-fails]
+  } else {
+    runs_seq = seq_len(runs)
   }
   
   ### define progress-bar
   pb = progress::progress_bar$new(
-    format = "MLE: [:bar] :percent complete, :eta ETA", 
-    total = runs, 
+    format = "Estimation: [:bar] :percent, :eta ETA", 
+    total = length(runs_seq), 
     clear = TRUE, 
-    width = 60, 
+    width = 45, 
     show_after = 0
     )
   
   ### start maximization
   start = Sys.time()
-	for (run in seq_len(runs)){
+	for (run in runs_seq){
 	  pb$tick(0)
 	  suppressWarnings({ tryCatch({ mods[[run]] = optimized(start_values[[run]])
     		                          if(mods[[run]][["code"]] %in% controls[["accept_codes"]] || controls[["at_true"]]){
@@ -97,45 +124,46 @@ init_est = function(controls){
   N  = controls[["states"]][2]
   scale_par = controls[["scale_par"]]
   
-  build_gammasUncon = function(dim){
+  build_Gamma = function(dim){
     Gamma = diag(dim)
-    diag(Gamma) = runif(dim,0.8,1)
+    diag(Gamma) = runif(dim,0.7,1)
     for(d in seq_len(dim)){
-      Gamma[d,-d] = runif(dim-1,0,0.2)
+      weights = sample.int(dim-1)
+      Gamma[d,-d] = (1-Gamma[d,d])*weights/sum(weights)
     }
-    return(Gamma2gammasUncon(Gamma))
+    return(Gamma)
   }
   
   if(controls[["model"]]=="HMM"){
-    gammasUncon = build_gammasUncon(M)
-    sigmasUncon = sigmaCon2sigmaUncon((seq(0,1,length.out=M)+runif(1))*scale_par[1])
+    gammasUncon = Gamma2gammasUncon(build_Gamma(M))
+    sigmasUncon = sigmaCon2sigmaUncon((seq(0.1,1,length.out=M)*runif(1))*scale_par[1])
     if(controls[["sdds"]][1] == "t"){
-      musUncon    = muCon2muUncon((seq(1,-1,length.out=M)+runif(1))*scale_par[1],link=FALSE)
-      dfs         = if(is.na(controls[["fixed_dfs"]][1])) runif(M,1,30) else integer(0)
+      musUncon    = muCon2muUncon((seq(1.1,-0.9,length.out=M)*runif(1))*scale_par[1],link=FALSE)
+      dfs         = if(is.na(controls[["fixed_dfs"]][1])) sample.int(30,M) else integer(0)
       thetaUncon  = c(gammasUncon,musUncon,sigmasUncon,dfs)
     }
     if(controls[["sdds"]][1] == "gamma"){
-      musUncon    = muCon2muUncon((seq(0,1,length.out=M)+runif(1))*scale_par[1],link=TRUE)
+      musUncon    = muCon2muUncon((seq(0.1,1,length.out=M)*runif(1))*scale_par[1],link=TRUE)
       thetaUncon  = c(gammasUncon,musUncon,sigmasUncon)
     }
   }
   
   if(controls[["model"]]=="HHMM"){
-    gammasUncon = build_gammasUncon(M)
-    sigmasUncon = sigmaCon2sigmaUncon((seq(0,1,length.out=M)+runif(1))*scale_par[1])
+    gammasUncon = Gamma2gammasUncon(build_Gamma(M))
+    sigmasUncon = sigmaCon2sigmaUncon((seq(0.1,1,length.out=M)*runif(1))*scale_par[1])
     if(controls[["sdds"]][1] == "t"){
-      musUncon = muCon2muUncon((seq(1,-1,length.out=M)+runif(1))*scale_par[1],link=FALSE)
-      dfs      = if(is.na(controls[["fixed_dfs"]][1])) runif(M,1,30) else integer(0)
+      musUncon = muCon2muUncon((seq(1.1,-0.9,length.out=M)*runif(1))*scale_par[1],link=FALSE)
+      dfs      = if(is.na(controls[["fixed_dfs"]][1])) sample.int(30,M) else integer(0)
       if(controls[["sdds"]][2] == "t"){
         gammasUncon_star = c()
         musUncon_star    = c()
         sigmasUncon_star = c()
         dfs_star         = c()
         for(m in seq_len(M)){
-          gammasUncon_star = c(gammasUncon_star,build_gammasUncon(N))
+          gammasUncon_star = c(gammasUncon_star,Gamma2gammasUncon(build_Gamma(N)))
           musUncon_star    = c(musUncon_star,sort(muCon2muUncon(muUncon2muCon(musUncon[m],link=FALSE)/scale_par[1]*scale_par[2]*runif(N,0.5,1.5),link=FALSE),decreasing=TRUE))
           sigmasUncon_star = c(sigmasUncon_star,sort(sigmaCon2sigmaUncon(sigmaUncon2sigmaCon(sigmasUncon[m])/scale_par[1]*scale_par[2]*runif(N,0.5,1.5)),decreasing=FALSE))
-          dfs_star         = c(dfs_star,if(is.na(controls[["fixed_dfs"]][2])) runif(N,1,30) else integer(0))
+          dfs_star         = c(dfs_star,if(is.na(controls[["fixed_dfs"]][2])) sample.int(30,N) else integer(0))
         }
         gammasUncon = c(gammasUncon,gammasUncon_star)
         musUncon    = c(musUncon,musUncon_star)
@@ -148,7 +176,7 @@ init_est = function(controls){
         musUncon_star    = c()
         sigmasUncon_star = c()
         for(m in seq_len(M)){
-          gammasUncon_star = c(gammasUncon_star,build_gammasUncon(N))
+          gammasUncon_star = c(gammasUncon_star,Gamma2gammasUncon(build_Gamma(N)))
           musUncon_star    = c(musUncon_star,sort(muCon2muUncon(abs(muUncon2muCon(musUncon[m],link=FALSE)/scale_par[1]*scale_par[2]*runif(N,0.5,1.5)),link=TRUE),decreasing=TRUE))
           sigmasUncon_star = c(sigmasUncon_star,sort(sigmaCon2sigmaUncon(sigmaUncon2sigmaCon(sigmasUncon[m])/scale_par[1]*scale_par[2]*runif(N,0.5,1.5)),decreasing=FALSE))
         }
@@ -159,17 +187,17 @@ init_est = function(controls){
       }
     }
     if(controls[["sdds"]][1] == "gamma"){
-      musUncon = muCon2muUncon((seq(0,1,length.out=M)+runif(1))*scale_par[1],link=FALSE)
+      musUncon = muCon2muUncon((seq(0.1,1,length.out=M)+runif(1))*scale_par[1],link=TRUE)
       if(controls[["sdds"]][2] == "t"){
         gammasUncon_star = c()
         musUncon_star    = c()
         sigmasUncon_star = c()
         dfs_star         = c()
         for(m in seq_len(M)){
-          gammasUncon_star = c(gammasUncon_star,build_gammasUncon(N))
+          gammasUncon_star = c(gammasUncon_star,Gamma2gammasUncon(build_Gamma(N)))
           musUncon_star    = c(musUncon_star,sort(muCon2muUncon(muUncon2muCon(musUncon[m]-mean(musUncon),link=FALSE)/scale_par[1]*scale_par[2]*runif(N,0.5,1.5),link=FALSE),decreasing=TRUE))
           sigmasUncon_star = c(sigmasUncon_star,sort(sigmaCon2sigmaUncon(sigmaUncon2sigmaCon(sigmasUncon[m])/scale_par[1]*scale_par[2]*runif(N,0.5,1.5)),decreasing=FALSE))
-          dfs_star         = c(dfs_star,if(is.na(controls[["fixed_dfs"]][2])) runif(N,1,30) else integer(0))
+          dfs_star         = c(dfs_star,if(is.na(controls[["fixed_dfs"]][2])) sample.int(30,N) else integer(0))
         }
         gammasUncon = c(gammasUncon,gammasUncon_star)
         musUncon    = c(musUncon,musUncon_star)
@@ -182,8 +210,8 @@ init_est = function(controls){
         musUncon_star    = c()
         sigmasUncon_star = c()
         for(m in seq_len(M)){
-          gammasUncon_star = c(gammasUncon_star,build_gammasUncon(N))
-          musUncon_star    = c(musUncon_star,sort(muCon2muUncon(muUncon2muCon(musUncon[m],link=FALSE)/scale_par[1]*scale_par[2]*runif(N,0.5,1.5),link=FALSE),decreasing=TRUE))
+          gammasUncon_star = c(gammasUncon_star,Gamma2gammasUncon(build_Gamma(N)))
+          musUncon_star    = c(musUncon_star,sort(muCon2muUncon(muUncon2muCon(musUncon[m],link=TRUE)/scale_par[1]*scale_par[2]*runif(N,0.5,1.5),link=TRUE),decreasing=TRUE))
           sigmasUncon_star = c(sigmasUncon_star,sort(sigmaCon2sigmaUncon(sigmaUncon2sigmaCon(sigmasUncon[m])/scale_par[1]*scale_par[2]*runif(N,0.5,1.5)),decreasing=FALSE))
         }
         gammasUncon = c(gammasUncon,gammasUncon_star)
@@ -218,7 +246,7 @@ nLL_hmm = function(thetaUncon,observations,controls){
   sigmas = thetaList[["sigmas"]]
   dfs    = thetaList[["dfs"]]
   
-  allprobs = matrix(0,nstates,T)
+  allprobs = matrix(NA,nstates,T)
   for (i in 1:nstates){
     if(sdd=="t"){
       allprobs[i,] = 1/sigmas[i]*dt((observations-mus[i])/sigmas[i],dfs[i])
@@ -262,7 +290,7 @@ nLL_hhmm = function(thetaUncon,observations,controls){
       allprobs[m,] = dgamma(observations_cs,shape=mus[m]^2/sigmas[m]^2,scale=sigmas[m]^2/mus[m])
     }
     for(t in seq_len(T)){
-      log_likelihoods[m,t] = -nLL_hmm(thetaUnconSplit[[m]],observations[t,-1][!is.na(observations[t,-1])],controls)
+      log_likelihoods[m,t] = -nLL_hmm(thetaUnconSplit[[m]],observations_fs[t,][!is.na(observations_fs[t,])],controls)
     }
   }
   
