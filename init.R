4ff146a93484a57bb58bc74a4311c83c37d46bf8
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-02-05 01:47:14 +0100

    source code

--- init.R
+++ init.R
@@ -1,36 +1,70 @@
 ### initialize the code
 load_code = function(){
-  if(!dir.exists("models")){
-    dir.create("models")
-  }
-  if(!dir.exists("data")){
-    dir.create("data")
-  }
+
   installed_packages = installed.packages()[,"Package"]
-  exe = alist(if(!"Rcpp" %in% installed_packages){ writeLines("\nInstalling package 'Rcpp'.\n"); install.packages("Rcpp",quite=TRUE)},
-              require("Rcpp"),
-              if(!"RcppArmadillo" %in% installed_packages){ writeLines("\nInstalling package 'RcppArmadillo'.\n"); install.packages("RcppArmadillo",quitely=TRUE)},
-              require("RcppArmadillo"),
-              if(!"progress" %in% installed_packages){ writeLines("\nInstalling package 'progress'.\n"); install.packages("progress",quite=TRUE)},
-              require("progress"),
-              if(!"MASS" %in% installed_packages){ writeLines("\nInstalling package 'MASS'.\n"); install.packages("MASS",quite=TRUE)},
-              require("MASS"),
-              if(!"tseries" %in% installed_packages){ writeLines("\nInstalling package 'tseries'.\n"); install.packages("tseries",quite=TRUE)},
-              require("tseries"),
-              Rcpp::sourceCpp("loglike.cpp"),
-              source("checks.R"),
-              source("data.R"),
-              source("exception.R"),
-              source("hhmmf.R"),
-              source("optim.R"),
-              source("trans.R"),
-              source("visual.R"),
-              source("viterbi.R"),
-              source("yahoo.R"))
+  exe = alist(
+    ### installing and loading packages
+    if(!"Rcpp" %in% installed_packages){ writeLines("\nInstalling package 'Rcpp'.\n"); install.packages("Rcpp",quite=TRUE)},
+    require("Rcpp"),
+    if(!"RcppArmadillo" %in% installed_packages){ writeLines("\nInstalling package 'RcppArmadillo'.\n"); install.packages("RcppArmadillo",quitely=TRUE)},
+    require("RcppArmadillo"),
+    if(!"progress" %in% installed_packages){ writeLines("\nInstalling package 'progress'.\n"); install.packages("progress",quite=TRUE)},
+    require("progress"),
+    if(!"MASS" %in% installed_packages){ writeLines("\nInstalling package 'MASS'.\n"); install.packages("MASS",quite=TRUE)},
+    require("MASS"),
+    if(!"tseries" %in% installed_packages){ writeLines("\nInstalling package 'tseries'.\n"); install.packages("tseries",quite=TRUE)},
+    require("tseries"),
+    
+    ### loading code
+    Rcpp::sourceCpp("scr/LL_HMM_Rcpp.cpp"),
+    Rcpp::sourceCpp("scr/LL_HHMM_Rcpp.cpp"),
+    source('R/apply_viterbi.R'),
+    source('R/check_controls.R'),
+    source('R/check_decoding.R'),
+    source('R/check_estimation.R'),
+    source('R/check_saving.R'),
+    source('R/compute_ci.R'),
+    source('R/compute_fs.R'),
+    source('R/create_visuals.R'),
+    source('R/download_data.R'),
+    source('R/exception.R'),
+    source('R/fhmm.R'),
+    source('R/Gamma2delta.R'),
+    source('R/Gamma2gammasCon.R'),
+    source('R/Gamma2gammasUncon.R'),
+    source('R/gammasCon2Gamma.R'),
+    source('R/gammasCon2gammasUncon.R'),
+    source('R/gammasUncon2Gamma.R'),
+    source('R/gammasUncon2gammasCon.R'),
+    source('R/init_est.R'),
+    source('R/max_likelihood.R'),
+    source('R/muCon2muUncon.R'),
+    source('R/muUncon2muCon.R'),
+    source('R/nLL_hhmm.R'),
+    source('R/nLL_hmm.R'),
+    source('R/plot_ll.R'),
+    source('R/plot_sdd.R'),
+    source('R/plot_ts.R'),
+    source('R/process_data.R'),
+    source('R/pseudo_residuals.R'),
+    source('R/read_data.R'),
+    source('R/sigmaCon2sigmaUncon.R'),
+    source('R/sigmaUncon2sigmaCon.R'),
+    source('R/simulate_data.R'),
+    source('R/states_decreasing.R'),
+    source('R/thetaCon2thetaList.R'),
+    source('R/thetaUncon2thetaCon.R'),
+    source('R/thetaUncon2thetaList.R'),
+    source('R/thetaUncon2thetaUnconSplit.R'),
+    source('R/thetaUnconSplit2thetaList.R')
+  )
   for(e in seq_len(length(exe))){
     suppressPackageStartupMessages(eval(exe[[e]]))
-    message(sprintf("Loading HHMM_Finance code: %.0f%%",(e/length(exe)*100)),"\r",appendLF=FALSE)
+    message(sprintf("loading fhmm code: %.0f%%",(e/length(exe)*100)),"\r",appendLF=FALSE)
   }
-  message(paste0("Data source: ",getwd(),"/data"))
-  message(paste0("Model results: ",getwd(),"/models"))
+  message(sprintf("fhmm code loaded %10s"," "))
+  
+  ### create and print path of folders
+  if(!dir.exists("models")) dir.create("models")
+  if(!dir.exists("data")) dir.create("data")
 }
