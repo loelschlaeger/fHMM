189076c0a6ef70a8c4fcff473bcf64e380dbca4a
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-02-06 13:18:21 +0100

    fixed bug

--- R/compute_ci.R
+++ R/compute_ci.R
@@ -2,44 +2,41 @@
 #'
 #' @param fit A fitted model
 #' @param controls A list of controls
-#' @param alpha Confidence level, default \code{alpha=0.95}
 #'
 #' @return A list containing the following elements:
-#' \item{lower_limit}{the lower limits of the intervals}
+#' \item{lb_alpha}{the lower bound of the intervals}
 #' \item{estimate}{the estimates}
-#' \item{upper_limit}{the upper limits of the intervals}
+#' \item{ub_limit}{the upper bound of the intervals}
 
-compute_ci = function(fit,controls,alpha=0.95){
-  ### input checks
-  if(alpha < 0 | alpha > 1){
-    stop(sprintf("%s (%s)",exception("F.5")[2],exception("F.5")[1]),call.=FALSE)
-  }
+compute_ci = function(fit,controls){
   if(is.null(controls[["controls_checked"]])){
     stop(sprintf("%s (%s)",exception("F.6")[2],exception("F.6")[1]),call.=FALSE)
   }
   
+  alpha = controls[["ci_level"]]
+  if(alpha < 0 | alpha > 1){
+    stop(sprintf("%s (%s)",exception("F.5")[2],exception("F.5")[1]),call.=FALSE)
+  }
+  
   ### extract estimates
   fisher = fit[["hessian"]]
-  estimates = thetaUncon2thetaCon(fit[["mod"]][["estimate"]],controls)
+  estimatesUncon = fit[["model"]][["estimate"]]
   
   ### compute confidence intervals using the inverse Hessian approach
   inv_fisher = MASS::ginv(fisher)
   sds = suppressWarnings(sqrt(diag(inv_fisher)))
-  lower_limit = estimates + qnorm(p = (1 - alpha) / 2) * sds
-  upper_limit = estimates + qnorm(p = 1 - (1 - alpha) / 2) * sds
+  lower_limit = estimatesUncon + qnorm(p = (1 - alpha) / 2) * sds
+  upper_limit = estimatesUncon + qnorm(p = 1 - (1 - alpha) / 2) * sds
   
   ### Hessian checks
-  eigen_values = eigen(inv_fisher)
-  lower_limit[which(eigen_values < 0)] = NA
-  upper_limit[which(eigen_values < 0)] = NA
-  if(any(eigen_values < 0)) {
-    warning("Inverse Hessian not positive definite. The confidence intervals for some parameters may be unreliable and are therefore replaced by NA.")
+  eigen_values = eigen(inv_fisher,only.values=TRUE,symmetric=TRUE)[["values"]]
+  lower_limit[eigen_values < 0] = NA
+  upper_limit[eigen_values < 0] = NA
+  out = lapply(list(lower_limit, estimatesUncon, upper_limit),thetaUncon2thetaCon,controls)
+  names(out) = c(paste0("lb_",alpha),"estimate",paste0("ub_",alpha))
+  if(any(eigen_values < 0) || any(is.na(out))) {
+    warning(sprintf("%s (%s)",exception("F.6")[2],exception("F.6")[1]),call.=FALSE)
   }
-  out = list(lower_limit = lower_limit, estimate = fit$estimate, upper_limit = upper_limit)
   
-  ### output checks
-  if(any(is.na(out))) {
-    warning(sprintf("%s (%s)",exception("C.3")[2],exception("C.3")[1]),call.=FALSE)
-  }
   return(out)
 }
