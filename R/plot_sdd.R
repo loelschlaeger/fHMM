bb5286a0fc4e05cacb4271e5c95ef8f1615d3424
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-02-20 00:29:31 +0100

    order thetaList when plotting sdds

--- R/plot_sdd.R
+++ R/plot_sdd.R
@@ -69,6 +69,7 @@ plot_sdd = function(controls,data,fit,decoding,colors){
         legend(legend=c("estimated","true"),col="grey",lwd=lwd,lty=c(1,2),cex=1.25,x="topright",bg=rgb(1,1,1,0.5))
       }
     }
+    thetaListOrdered = thetaList2thetaListOrdered(thetaList,controls)
     if(controls[["model"]]=="HMM"){
       pdf(file = filename, width=8, height=8)
         if(controls[["sim"]]){
@@ -79,9 +80,9 @@ plot_sdd = function(controls,data,fit,decoding,colors){
           sdd_true_parm = NULL
         }
         create_sdds_plot(nostates      = controls[["states"]][1],
-                         mus           = fit[["thetaList"]][["mus"]],
-                         sigmas        = fit[["thetaList"]][["sigmas"]],
-                         dfs           = fit[["thetaList"]][["dfs"]],
+                         mus           = thetaListOrdered[["mus"]],
+                         sigmas        = thetaListOrdered[["sigmas"]],
+                         dfs           = thetaListOrdered[["dfs"]],
                          sdd           = controls[["sdds"]][1],
                          x_range       = c(min(data[["logReturns"]]),max(data[["logReturns"]])),
                          colors        = colors[["HMM"]],
@@ -97,11 +98,11 @@ plot_sdd = function(controls,data,fit,decoding,colors){
                                "dfs"    = data[["thetaList0"]][["dfs"]])
         } else {
           sdd_true_parm = NULL
-        } 
+        }
         create_sdds_plot(nostates      = controls[["states"]][1],
-                         mus           = fit[["thetaList"]][["mus"]],
-                         sigmas        = fit[["thetaList"]][["sigmas"]],
-                         dfs           = fit[["thetaList"]][["dfs"]],
+                         mus           = thetaListOrdered[["mus"]],
+                         sigmas        = thetaListOrdered[["sigmas"]],
+                         dfs           = thetaListOrdered[["dfs"]],
                          sdd           = controls[["sdds"]][1],
                          x_range       = c(min(data[["logReturns"]][,1]),max(data[["logReturns"]][,1])),
                          colors        = colors[["HHMM_cs"]],
@@ -110,9 +111,9 @@ plot_sdd = function(controls,data,fit,decoding,colors){
         xlims = matrix(0,nrow=2,ncol=controls[["states"]][1])
         for(cs in seq_len(controls[["states"]][1])){
           xlims[,cs] = create_sdds_plot(nostates = controls[["states"]][2],
-                                        mus      = fit[["thetaList"]][["mus_star"]][[cs]],
-                                        sigmas   = fit[["thetaList"]][["sigmas_star"]][[cs]],
-                                        dfs      = fit[["thetaList"]][["dfs_star"]][[cs]],
+                                        mus      = thetaListOrdered[["mus_star"]][[cs]],
+                                        sigmas   = thetaListOrdered[["sigmas_star"]][[cs]],
+                                        dfs      = thetaListOrdered[["dfs_star"]][[cs]],
                                         sdd      = controls[["sdds"]][2],
                                         x_range  = c(min(data[["logReturns"]][,-1],na.rm=TRUE),max(data[["logReturns"]][,-1],na.rm=TRUE)),
                                         c_xlim   = TRUE) 
@@ -126,9 +127,9 @@ plot_sdd = function(controls,data,fit,decoding,colors){
             sdd_true_parm = NULL
           } 
           create_sdds_plot(nostates      = controls[["states"]][2],
-                           mus           = fit[["thetaList"]][["mus_star"]][[cs]],
-                           sigmas        = fit[["thetaList"]][["sigmas_star"]][[cs]],
-                           dfs           = fit[["thetaList"]][["dfs_star"]][[cs]],
+                           mus           = thetaListOrdered[["mus_star"]][[cs]],
+                           sigmas        = thetaListOrdered[["sigmas_star"]][[cs]],
+                           dfs           = thetaListOrdered[["dfs_star"]][[cs]],
                            sdd           = controls[["sdds"]][2],
                            x_range       = c(min(data[["logReturns"]][,-1],na.rm=TRUE),max(data[["logReturns"]][,-1],na.rm=TRUE)),
                            c_xlim        = FALSE,
