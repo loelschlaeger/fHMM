7bab2741b3da67118cdb4b719d2791cdb3d636e6
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-02-25 10:06:33 +0100

    shifting extreme transition probabilities

--- R/Gamma2gammasCon.R
+++ R/Gamma2gammasCon.R
@@ -1,7 +1,10 @@
 #' Get constrained non-diagonal matrix elements of transition probability matrix
+#' @details Function shifts 0 and 1 non-diagonal elements by 1e-3.
 #' @param Gamma Transition probability matrix
 #' @return Constrained non-diagonal matrix elements (column-wise)
 Gamma2gammasCon = function(Gamma){
-  diag(Gamma) = NA
-  return(Gamma[!is.na(Gamma)])
+  gammasCon = Gamma[row(Gamma)!=col(Gamma)] 
+  gammasCon = replace(gammasCon,gammasCon==0,1e-3)
+  gammasCon = replace(gammasCon,gammasCon==1,1-1e-3)
+  return(gammasCon)
 }
