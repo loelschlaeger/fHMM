1324eda8804888085c9ed309dc1d5dd1d5810a25
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-03-13 13:24:37 +0100

    NA to NULL

--- R/check_saving.R
+++ R/check_saving.R
@@ -1,20 +1,20 @@
 #' @title Saving check
 #' @description Checks for overwriting and saves model results.
-#' @param object An .rds-object to be saved, default \code{NA}.
-#' @param name A character, the name of the object to be saved, default \code{NA}.
+#' @param object An .rds-object to be saved, default \code{NULL}.
+#' @param name A character, the name of the object to be saved, default \code{NULL}.
 #' @param filetype A character, the filetype of the object to be saved.
 #' @param controls A list of controls.
 #' @return A boolean, determining whether saving is possible or not. If \code{filetype="rds"}, \code{object} is saved.
 
-check_saving = function(object=NA,name=NA,filetype,controls){
-  if(is.na(object) && is.na(name)){
+check_saving = function(object=NULL,name=NULL,filetype,controls){
+  if(is.null(object) & is.null(name)){
     stop("Either 'object' or 'name' has to be specified.")
   }
   
   ### specify saving path
   path = paste0(controls[["path"]],"/models/",controls[["id"]])
   if(!dir.exists(path)) dir.create(path)
-  if(any(!is.na(object))) name = deparse(substitute(object))
+  if(!is.null(object)) name = deparse(substitute(object))
   filename = paste0(path,"/",name,".",filetype)
   
   ### check for overwriting
