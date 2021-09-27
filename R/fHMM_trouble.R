0d815884c324a1c5089f87c9825c57fc5a0f6bda
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-09-27 23:08:14 +0200

    Unified fHMM_trouble.

--- R/fHMM_trouble.R
+++ R/fHMM_trouble.R
@@ -15,7 +15,7 @@
 #' trouble("S.1")
 #' @export
 
-trouble = function(code){
+solve_trouble = function(code){
   
   ### data frame of exceptions
   exceptions = data.frame(rbind(
@@ -72,4 +72,21 @@ trouble = function(code){
   } else {
     message("Error code unknown.")
   }
+}
+
+#' Print method for \code{fHMM_trouble}.
+#' @description 
+#' This function is the print method for an object of class \code{fHMM_trouble}.
+#' @param x
+#' An object of class \code{fHMM_trouble}.
+#' @param ...
+#' Ignored.
+#' @return
+#' Returns invisibly \code{x}.
+#' @export
+
+print.fHMM_trouble = function(x, ...) {
+  cat(x$code, ":", x$response, "\n")
+  cat(x$debugging, "\n")
+  return(invisible(x))
 }
