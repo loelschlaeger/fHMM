2ade79497b928381b3934d77ea4f9d1ca8923177
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-10-01 12:05:36 +0200

    Function to check correct date format.

---
+++ tests/testthat/test-check_date.R
@@ -0,0 +1,7 @@
+test_that("checks of format 'YYYY-MM-DD' for dates work", {
+  error_code = "C7"
+  expect_equal(check_date(date = "2000-01-01"), as.Date("2000-01-01"))
+  expect_error(check_date(date = "2000-02-30"), error_code)
+  expect_error(check_date(date = "2000-13-01"), error_code)
+  expect_error(check_date(date = "01.01.2021"), error_code)
+})
