443da43a268d0c43e1978e02522c1f30bd7d6218
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-10-15 17:13:42 +0200

    Implemented print function for fHMM_model.

--- R/print.fHMM_model.R
+++ R/print.fHMM_model.R
@@ -1,8 +1,19 @@
-print.fHMM_model = function() {
-  writeLines(paste0("Estimation results of model '",controls[["id"]],"':\n"))
-  writeLines(sprintf("%-15s %.2f","log-likelihood:",fit[["logLikelihood"]]))
-  writeLines(sprintf("%-15s %.2f","AIC:",fit[["AIC"]]))
-  writeLines(sprintf("%-15s %.2f","BIC:",fit[["BIC"]]))
-  writeLines(sprintf("%-15s %.0f","exit code:",mod[["code"]]))
-  writeLines(sprintf("%-15s %.0f","iterations:",mod[["iterations"]])); cat("\n")
+#' Print method for \code{fHMM_model}.
+#' @description 
+#' This function is the print method for an object of class \code{fHMM_model}.
+#' @param x
+#' An object of class \code{fHMM_model}.
+#' @param ...
+#' Ignored.
+#' @return
+#' Returns \code{x} invisibly.
+#' @export
+
+print.fHMM_model = function(x) {
+  cat("fHMM fitted model:\n")
+  cat("* total estimation time:", x$estimation_time, units(x$estimation_time), 
+      "\n")
+  cat("* accepted runs:", sum(!is.na(x$lls)), "of", length(x$lls), "\n")
+  cat("* log-likelihood", x$ll, "\n")
+  return(invisible(x))
 }
