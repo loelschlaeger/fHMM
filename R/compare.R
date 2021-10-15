c8e94e605ceb7450d24b365006503607e75dfcb7
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-10-15 17:13:52 +0200

    Initialized comparison function.

---
+++ R/compare.R
@@ -0,0 +1,11 @@
+#' Comparison of multiple \code{fHMM_model}.
+#' @description 
+#' This function compares multiple \code{fHMM_model}.
+#' @param ...
+#' A list of one or more objects of class \code{fHMM_model}.
+#' @return
+#' A data frame.
+
+compare = function(...) {
+  
+}
