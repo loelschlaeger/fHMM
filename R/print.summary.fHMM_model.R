7b26b2d1cdf873a25f7d5aeb2ff5ba86441a3b0a
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-06 16:23:24 +0100

    Implemented model summary.

--- R/print.summary.fHMM_model.R
+++ R/print.summary.fHMM_model.R
@@ -8,60 +8,20 @@
 #' @export
 
 print.summary.fHMM_model = function(x, digits = 4, ...) {
-  cat("Summary of fHMM model\n")
+  cat("Summary of fHMM model\n\n")
+  print(x$model_info)
+  cat("\nState-dependent distributions:\n")
+  print(x$sdds)
+  cat("\n")
+  cat("\nEstimates:\n")
   print(x$estimates, digits = digits)
-  
-  ### frequency of decoded states
-  writeLines("Frequency of decoded states\n")
-  if(controls[["model"]] == "hmm"){ 
-    out = table((decoding))
-    names(out) = paste("state",names(out))
-    print(out)
-    cat("\n")
+  if(!is.null(x$decoding_table)){
+    cat("\nStates:\n")
+    print(x$decoding_table, digits = digits)
   }
-  if(controls[["model"]]=="hhmm"){
-    out_cs = table(factor(decoding[,1],levels = seq_len(controls[["states"]][1])))
-    names(out_cs) = paste("CS state",names(out_cs))
-    print(out_cs)
-    cat("\n")
-    for(state in seq_len(controls[["states"]][1])){
-      writeLines(paste0("Conditional on CS state ",state,":"))
-      out_fs = table(factor(decoding[decoding[,1]==state,-1],levels = seq_len(controls[["states"]][2])))
-      names(out_fs) = paste("FS state",names(out_fs))
-      print(out_fs)
-      cat("\n")
-    }
+  if(!is.null(x$res_summary)){
+    cat("\nResiduals:\n")
+    print(x$res_summary, digits = digits)
   }
-  
-  ### comparison between true states and predicted states
-  if(controls[["sim"]]){
-    compare_true_predicted_states = function(no_states,decoded_states,true_states,label=NULL){
-      c_table = matrix(0,no_states,no_states)
-      rownames(c_table) = paste0("true ",label,"state ",seq_len(no_states))
-      colnames(c_table) = paste0("decoded ",label,"state ",seq_len(no_states))
-      for(i in seq_len(no_states)) for(j in seq_len(no_states)){
-        value = sum(decoded_states==i & true_states==j, na.rm = TRUE) / sum(decoded_states==i, na.rm = TRUE) * 100
-        c_table[i,j] = if(is.nan(value)) "NA" else sprintf("%.0f%%",value)
-      }
-      print(c_table,quote=FALSE,right=TRUE)
-    }
-    writeLines("Comparison between true and decoded states\n")
-    if(controls[["model"]]=="hmm"){
-      compare_true_predicted_states(controls[["states"]][1],decoding,data[["states0"]])
-    }
-    if(controls[["model"]]=="hhmm"){
-      compare_true_predicted_states(controls[["states"]][1],decoding[,1],data[["states0"]][,1],label="CS ")
-      writeLines("")
-      for(cs_state in seq_len(controls[["states"]][1])){
-        writeLines(paste0("Conditional on true CS state ",cs_state,":"))
-        compare_true_predicted_states(controls[["states"]][2],decoding[data[["states0"]][,1]==cs_state,-1],data[["states0"]][data[["states0"]][,1]==cs_state,-1],label="FS ")
-        writeLines("")
-      }
-    }
-  }
-  
-  
-  
   return(invisible(x))
-  
 }
--- R/summary.fHMM_model.R
+++ R/summary.fHMM_model.R
@@ -11,6 +11,7 @@
 
 summary.fHMM_model = function(object, ci_level = 0.05, ...) {
   
+  ### model information
   simulated = object$data$controls$simulated
   hierarchy = object$data$controls$hierarchy
   no_par = length(object$estimate)
@@ -18,37 +19,49 @@ summary.fHMM_model = function(object, ci_level = 0.05, ...) {
   ll = object$ll
   aic = -2*ll + 2*no_par
   bic = -2*ll + no_par*log(data_size)
+  model_info = data.frame(
+    simulated, hierarchy, "LL" = ll, "AIC" = aic, "BIC" = bic,
+    row.names = "value"
+  )
   
-  ### build data frame of parameter estimates
+  ### state-dependent distributions
+  sdds = parUncon2par(object$estimate, object$data$controls)$sdds
+  
+  ### parameter estimates
   ci = compute_ci(object, ci_level)
-  ci$lb$sdds = NULL
-  ci$estimate$sdds = NULL
-  ci$ub$sdds = NULL
-  estimates = data.frame(lapply(ci,unlist))
+  estimates_table = data.frame(lapply(ci, as.vector))
   if(simulated){
-    true = object$data$true_parameters
-    true$sdds = NULL
-    estimates = cbind(estimates, true = unlist(true))
+    true = par2parCon(object$data$true_parameters, object$data$controls)
+    estimates_table = cbind(estimates_table, true = as.vector(true))
   }
   
-  ### decoded states
+  ### states
   if(!is.null(object$decoding)){
-    decod_freq = table(object$decoding)   
     if(simulated){
-      true_decod_freq = table(object$data$markov_chain)
+      decoding_table = table(object$data$markov_chain, object$decoding,
+                             dnn = c("true", "decoded"))
+    } else {
+      decoding_table = table(object$decoding, dnn = "decoded")
     }
+  } else {
+    decoding_table = NULL
+  }
+  
+  ### residuals
+  if(!is.null(object$residuals)){
+    res_summary = summary(object$residuals)
+  } else {
+    res_summary = NULL
   }
  
   ### build and return summary
-  out = list("simulated" = simulated,
-             "hierarchy" = hierarchy,
-             "no_par" = no_par,
+  out = list("no_par" = no_par,
              "data_size" = data_size,
-             "ll" = ll,
-             "aic" = aic,
-             "bic" = bic,
-             "estimates" = estimates,
-             "decod_freq" = decod_freq)
+             "model_info" = model_info,
+             "sdds" = sdds,
+             "estimates_table" = estimates_table,
+             "decoding_table" = decoding_table,
+             "res_summary" = res_summary)
   class(out) = "summary.fHMM_model"
   return(out)
 }
