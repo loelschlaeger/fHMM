7bdff85e3ae3e1e8ce23a1e474ff9607471ca055
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-22 23:37:27 +0100

    Updated documentation.

--- R/check_date.R
+++ R/check_date.R
@@ -1,15 +1,15 @@
-#' Check the correct date format "YYYY-MM-DD".
+#' Check date format "YYYY-MM-DD".
 #' @description 
 #' This function checks if the input \code{date} has the format "YYYY-MM-DD".
 #' @param date
 #' A character, specifying a date in format "YYYY-MM-DD".
 #' @return
-#' \code{as.date(date)} if \code{date} has the correct format. Otherwise, the
-#' function throws an error.
+#' \code{as.Date(date)} if \code{date} has the format "YYYY-MM-DD". 
+#' Otherwise, the function throws an error.
 
 check_date = function(date) {
   date = try(as.Date(date), silent = TRUE)
-  if(class(date) == "try-error" || is.na(as.Date(date, format="%Y/%m/%d")))
+  if(class(date) == "try-error" || is.na(as.Date(date, format="%Y-%m-%d")))
     stop("C7")
   return(date)
 }
