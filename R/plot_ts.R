ee612a5a613b0bb9f4642fbc710d8c61900ff3dd
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-22 23:38:26 +0100

    Added display of events.

--- R/plot_ts.R
+++ R/plot_ts.R
@@ -18,11 +18,24 @@
 plot_ts = function(data, decoding = NULL, colors = NULL, events = NULL, 
                    predict = NULL){
   
+  ### helper functions
+  add_events = function(events, labels = FALSE){
+    abline(v = as.Date(events$dates))
+    if(labels){
+      axis(3, as.Date(events$dates), seq_along(events$dates))
+      mtext(paste0(seq_along(events$dates),": ",events$names, collapse = "; "), 
+            side = 3, line = 2)
+    }
+  }
+  
   if(!data$controls$hierarchy){
     
     if(data$controls$simulated){
+      
+      par(las = 1)
   
-      plot(x = data$time_points, y = data$data, type = "h")
+      plot(x = data$time_points, y = data$data, type = "h", col = "grey",
+           xlab = "time points", ylab = "", main = "Data time series")
       
     } else {
       
@@ -30,13 +43,17 @@ plot_ts = function(data, decoding = NULL, colors = NULL, events = NULL,
       
       layout(matrix(1:2, nrow = 2))
    
-      plot(x = as.Date(data$dates), y = data$time_series, type = "l",
+      plot(x = as.Date(data$dates), y = data$time_series, type = "l", 
+           col = "grey",
            xaxt = "n", xlab = "", ylab = "")
       
+      add_events(events)
+      
       plot(x = as.Date(data$dates), y = data$data, type = "h", xlab = "",
+           col = "grey",
            ylab = "")
       
-      
+      add_events(events, labels = TRUE)
       
     }
     
