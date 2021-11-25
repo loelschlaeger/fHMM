590678aa023a2d9ee4e092815fdaba9ff6f9e80c
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-25 10:41:09 +0100

    Update.

--- sandbox.R
+++ sandbox.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 library(magrittr)
 devtools::load_all()
-#install.packages("fHMM_0.3.0.9000.tar.gz", repos = NULL, type = "source", INSTALL_opts = c('--no-lock'))
+#install.packages("../fHMM_0.3.0.9000.tar.gz", repos = NULL, type = "source", INSTALL_opts = c('--no-lock'))
 
 ### download data -----------------------------------------------------------
 download_data(symbol = "^GDAXI", file = "dax.csv", verbose = FALSE)
@@ -33,60 +33,63 @@ predict(model, ahead = 10)
 
 ### empirical HMM -----------------------------------------------------------
 controls = list(
-  states  = 2,
-  sdds    = "t",
-  data    = list(file        = "dax.csv",
-                 date_column = "Date",
-                 data_column = "Close",
-                 logreturns  = TRUE)
+  states = 2,
+  sdds   = "t",
+  data   = list(file        = "dax.csv",
+                date_column = "Date",
+                data_column = "Close",
+                logreturns  = TRUE)
 )
 controls %<>% set_controls
 data = prepare_data(controls)
 summary(data)
-plot(data)
+events = list(dates = c("2001-09-11", "2008-09-15", "2020-01-27"),
+              labels = c("9/11 terrorist attack", "Bankruptcy Lehman Brothers",
+                         "First COVID-19 case Germany")) %>% fHMM_events
+plot(data, events)
 model = fit_model(data, ncluster = 7) %>%
   decode_states %>%
   compute_residuals
 summary(model)
-events = list(dates = c("2001-09-11", "2008-09-15", "2020-01-27"),
-              labels = c("9/11 terrorist attack", "Bankruptcy Lehman Brothers",
-                         "First COVID-19 case Germany")) %>% fHMM_events
 model %>% plot("ll")
 model %>% plot("sdds")
-model %<>% reorder_states(state_order = 2:1)
 model %>% plot("pr")
 model %>% plot("ts", events)
 
 ### simulated HHMM ----------------------------------------------------------
 controls = list(
   hierarchy = TRUE,
-  states = c(2,4),
-  horizon   = c(100, 30)
+  states    = c(2,2),
+  sdds      = c("t(mu = -1|1, sigma = 0.1, df = Inf)", 
+                "t(mu = -1|1, sigma = 0.1, df = Inf)"),
+  horizon   = c(100,30),
+  fit       = list("runs" = 10)
 )
-controls = set_controls(controls)
+controls %<>% set_controls
 data = prepare_data(controls)
-model = fit_model(data)
+#TODO: plot(data)
+model = fit_model(data, ncluster = 7)
+#TODO: model %<>% decode_states
+#TODO: model %<>% compute_residuals
 summary(model)
 compare(model)
-plot(model, plot_type = "ts")
+model %>% plot("ll")
+#TODO: model %>% plot("sdds")
+#TODO: model %>% plot("pr")
+#TODO: model %>% plot("ts", events)
 
 ### empirical HHMM ----------------------------------------------------------
 controls = list(
   hierarchy = TRUE,
-  horizon   = c(100, NA),
-  sdds      = c("t(df = 1)", "t(df = 1)"),
-  period    = "m",
-  data      = list(file        = c("dax.csv","vw.csv"),
-                   logreturns  = c(TRUE,TRUE))
+  horizon = c(100, NA),
+  sdds = c("t(df = 1)", "t(df = 1)"),
+  period = "m",
+  data = list(file = c("dax.csv","vw.csv"),
+              logreturns = c(TRUE,TRUE))
 )
 controls = set_controls(controls)
 data = prepare_data(controls)
 model = fit_model(data)
 summary(model)
-events = list(
-  dates = c("2001-09-11","2008-09-15","2020-01-27"),
-  names = c("9/11 terrorist attack","Bankruptcy of Lehman Brothers",
-            "First COVID-19 case in Germany")
-)
 
 
