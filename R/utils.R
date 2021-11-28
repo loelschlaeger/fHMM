bcf81d1b263dc2b82c57f409545d79129093d518
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-28 14:32:44 +0100

    Put utils functions together.

---
+++ R/utils.R
@@ -0,0 +1,128 @@
+#' Check date format "YYYY-MM-DD".
+#' @description
+#' This function checks if the input \code{date} has the format "YYYY-MM-DD".
+#' @param date
+#' A character, specifying a date in format "YYYY-MM-DD".
+#' @return
+#' \code{as.Date(date)} if \code{date} has the format "YYYY-MM-DD".
+#' Otherwise, the function throws an error.
+
+check_date <- function(date) {
+  date <- try(as.Date(date), silent = TRUE)
+  if (class(date) == "try-error" || is.na(as.Date(date, format = "%Y-%m-%d"))) {
+    stop("C7")
+  }
+  return(date)
+}
+
+#' Check if \code{x} is a ((non)-negative) ((non-)positive) (integer) numeric
+#' (vector).
+#' @param x
+#' An R object.
+#' @param int
+#' A boolean, if \code{TRUE} checks if \code{x} is an integer.
+#' @param neg
+#' A boolean, if \code{TRUE} checks if \code{x} is negative.
+#' @param non_neg
+#' A boolean, if \code{TRUE} checks if \code{x} is non-negative.
+#' @param pos
+#' A boolean, if \code{TRUE} checks if \code{x} is positive.
+#' @param non-pos
+#' A boolean, if \code{TRUE} checks if \code{x} is non-positive.
+#' @return
+#' A boolean.
+
+is_number <- function(x, int = FALSE, neg = FALSE, non_neg = FALSE, pos = FALSE,
+                      non_pos = FALSE) {
+  if (length(x) == 0) {
+    return(TRUE)
+  }
+  for (i in 1:length(x)) {
+    out <- rep(TRUE, length(x))
+    if (!is.numeric(x[i])) {
+      out[i] <- FALSE
+      break
+    }
+    if (int) {
+      if (x[i] %% 1 != 0) {
+        out[i] <- FALSE
+        break
+      }
+    }
+    if (neg) {
+      if (!x[i] < 0) {
+        out[i] <- FALSE
+        break
+      }
+    }
+    if (non_neg) {
+      if (!x[i] >= 0) {
+        out[i] <- FALSE
+        break
+      }
+    }
+    if (pos) {
+      if (!x[i] > 0) {
+        out[i] <- FALSE
+        break
+      }
+    }
+    if (non_pos) {
+      if (!x[i] <= 0) {
+        out[i] <- FALSE
+        break
+      }
+    }
+  }
+  return(out)
+}
+
+#' Check if \code{x} is a transition probability matrix.
+#' @param x
+#' A matrix.
+#' @return
+#' A boolean.
+
+is_tpm <- function(x) {
+  if (nrow(x) != ncol(x) ||
+      any(abs(rowSums(x) - 1) > .Machine$double.eps) ||
+      any(x < 0)) {
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
+}
+
+#' Match positions of two numeric vectors "as good as possible".
+#' @description
+#' This function matches the positions of two numeric vectors.
+#' @param x
+#' A numeric vector.
+#' @param y
+#' Another numeric vector of the same length as \code{x}.
+#' @return
+#' An integer vector of length \code{length(x)} with the positions of \code{y}
+#' in \code{x}.
+
+match_all <- function(x, y) {
+  stopifnot(length(x) == length(y))
+  matches <- numeric(length(x))
+  distances <- unique(sort(dist(c(x, y)))) + sqrt(.Machine$double.eps)
+  for (d in distances) {
+    if (any(c(!is.na(x), !is.na(y)))) {
+      for (i_x in 1:length(x)) {
+        for (i_y in 1:length(y)) {
+          if (!is.na(x[i_x]) && !is.na(y[i_y])) {
+            if (isTRUE(all.equal(x[i_x], y[i_y], d))) {
+              matches[i_y] <- i_x
+              x[i_x] <- NA
+              y[i_y] <- NA
+            }
+          }
+        }
+      }
+    }
+  }
+  return(matches)
+}
+
