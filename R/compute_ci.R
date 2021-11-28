08fad3138cea3210491b1f93b0643c1337343db1
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-28 11:02:52 +0100

    Solve numerical issue when entry in Hessian is -Inf, Inf, NA or NaN

--- R/compute_ci.R
+++ R/compute_ci.R
@@ -21,6 +21,7 @@ compute_ci <- function(x, ci_level = 0.05) {
   }
 
   ### compute confidence intervals using the inverse Hessian approach
+  x$hessian[which(x$hessian %in% c(Inf,-Inf,NA,NaN))] <- 0
   inv_fisher <- MASS::ginv(x$hessian)
   sds <- suppressWarnings(sqrt(diag(inv_fisher)))
   lower_limit <- x$estimate + qnorm(p = (1 - ci_level) / 2) * sds
