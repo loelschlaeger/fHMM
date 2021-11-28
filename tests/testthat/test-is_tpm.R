53a6c2f31fadb65974b7f3b3974ca16ace94c657
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-28 14:34:32 +0100

    Merged tests.

--- tests/testthat/test-is_tpm.R
+++
@@ -1,5 +0,0 @@
-test_that("check for tpm works", {
-  expect_true(is_tpm(matrix(c(0.8, 0.1, 0.2, 0.9), 2, 2)))
-  expect_false(is_tpm(matrix(c(0.8, 0.2, 0.2, 0.9), 2, 2)))
-  expect_false(is_tpm(matrix(c(0.8, -0.2, 0.2, 0.9), 2, 2)))
-})
--- tests/testthat/test-match_all.R
+++
@@ -1,3 +0,0 @@
-test_that("brute force matching works", {
-  expect_equal(match_all(1:9, 9:1), 9:1)
-})
