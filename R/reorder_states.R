4eee176a8f13c772cfcb9078af90039d3809b273
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-28 15:05:31 +0100

    Reorder of states for HHMM.

--- R/reorder_states.R
+++ R/reorder_states.R
@@ -12,18 +12,17 @@
 #'         values from \code{1} to \code{x$data$controls$states}. If the old
 #'         state number \code{x} should be the new state number \code{y}, put
 #'         the value \code{x} at the position \code{y} of \code{state_order}.
-#'         E.g. for a 2-state HMM, specifying \code{state_order = c(2,1)}
-#'         swaps the states.
+#'         E.g. for a 2-state HMM, specifying \code{state_order = c(2,1)} swaps
+#'         the states.
 #'   \item If \code{x$data$controls$hierarchy = TRUE}, \code{state_order} must
 #'         be a matrix of dimension \code{x$data$controls$states[1]} x
 #'         \code{x$data$controls$states[2] + 1}. The first column orders the
 #'         coarse-scale states with the logic as described above. For each row,
-#'         the second to last elements order the fine-scale states of the
-#'         coarse-scale state specified by the first element. E.g. for an HHMM
-#'         with 2 coarse- and 2 fine-scale states, specifying
-#'         \code{state_order = matrix(c(1,2,2,1,1,2),2,3)} swaps both
-#'         coarse-scale states and the fine-scale states for coarse-scale state
-#'         2.
+#'         the elements from second to last position order the fine-scale states 
+#'         of the coarse-scale state specified by the first element. E.g. for an 
+#'         HHMM with 2 coarse-scale and 2 fine-scale states, specifying
+#'         \code{state_order = matrix(c(2,1,2,1,1,2),2,3)} swaps the
+#'         coarse-scale states and the fine-scale states of coarse-scale state 2.
 #' }
 #' @return
 #' An object of class \code{fHMM_model}.
@@ -39,42 +38,50 @@ reorder_states <- function(x, state_order) {
     if (!(is.numeric(state_order) &&
       length(state_order) == x$data$controls$states &&
       all(state_order %in% 1:x$data$controls$states))) {
-      stop("<'state_order' missspecified>")
+      stop("'state_order' missspecified>")
     }
+    state_order = as.matrix(state_order)
   } else {
     if (!(is.numeric(state_order) && is.matrix(state_order) &&
       all(dim(state_order) == x$data$controls$states + c(0, 1)) &&
       all(state_order[1, ] %in% 1:x$data$controls$states[1]) &&
       all(state_order[-1, ] %in% 1:x$data$controls$states[2]))) {
-      stop("<'state_order' missspecified>")
+      stop("'state_order' missspecified>")
     }
   }
 
   ### reorder states
-  if (!x$data$controls$hierarchy) {
-    par <- parUncon2par(x$estimate, x$data$controls)
-    permut <- diag(x$data$controls$states)[state_order, ]
-    par$Gamma <- permut %*% par$Gamma %*% t(permut)
-    par$mus <- as.vector(permut %*% par$mus)
-    par$sigmas <- as.vector(permut %*% par$sigmas)
+  par <- parUncon2par(x$estimate, x$data$controls)
+  permut <- diag(x$data$controls$states[1])[state_order[,1], ]
+  par$Gamma <- permut %*% par$Gamma %*% t(permut)
+  par$mus <- as.vector(permut %*% par$mus)
+  par$sigmas <- as.vector(permut %*% par$sigmas)
+  if(x$data$controls$sdds[[1]]$name == "t")
     par$dfs <- as.vector(permut %*% par$dfs)
-    parUncon <- par2parUncon(par, x$data$controls)
-    permut_all <- diag(length(x$estimate))[match_all(x$estimate, parUncon), ]
-    x$estimate <- parUncon
-    x$gradient <- as.vector(permut_all %*% x$gradient)
-    x$hessian <- permut_all %*% x$hessian %*% t(permut_all)
-    x$nlm_output$estimate <- x$estimate
-    x$nlm_output$gradient <- x$gradient
-  } else {
-    stop("not implemented yet.")
+  if(x$data$controls$hierarchy){
+    for(s in state_order[,1]){
+      permut <- diag(x$data$controls$states[2])[state_order[which(state_order[,1] == 2),-1], ]
+      par$Gammas_star[[s]] <- permut %*% par$Gammas_star[[s]] %*% t(permut)
+      par$mus_star[[s]] <- as.vector(permut %*% par$mus_star[[s]])
+      par$sigmas_star[[s]] <- as.vector(permut %*% par$sigmas_star[[s]])
+      if(x$data$controls$sdds[[2]]$name == "t")
+        par$dfs_star[[s]] <- as.vector(permut %*% par$dfs_star[[s]])
+    }
   }
+  parUncon <- par2parUncon(par, x$data$controls)
+  permut_all <- diag(length(x$estimate))[match_all(x$estimate, parUncon), ]
+  x$estimate <- parUncon
+  x$gradient <- as.vector(permut_all %*% x$gradient)
+  x$hessian <- permut_all %*% x$hessian %*% t(permut_all)
+  x$nlm_output$estimate <- x$estimate
+  x$nlm_output$gradient <- x$gradient
 
   ### redo decoding and residual computation
   if (!is.null(x$decoding)) {
-    x <- decode_states(x)
+    x <- suppressMessages(decode_states(x))
   }
   if (!is.null(x$residuals)) {
-    x <- compute_residuals(x)
+    x <- suppressMessages(compute_residuals(x))
   }
 
   ### return reorderd 'fHMM_model'
