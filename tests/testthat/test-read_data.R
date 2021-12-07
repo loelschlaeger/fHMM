4abdc494550d88d8cc0489e872f2735118559afb
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-12-07 19:12:57 +0100

    Updated Vignettes.

--- tests/testthat/test-read_data.R
+++ tests/testthat/test-read_data.R
@@ -1,22 +1,5 @@
 test_that("read of data works", {
-  skip_if_offline()
 
-  ### HMM with DAX data
-  file <- paste0(tempdir(), "/dax.csv")
-  sink(tempfile())
-  download_data(symbol = "^GDAXI", file = file)
-  sink()
-  controls <- list(
-    data = list(
-      file = file,
-      data_column = "Close",
-      to = "2021-10-01"
-    )
-  )
-  controls <- set_controls(controls)
-  expect_snapshot(read_data(controls))
-
-  ### HMM with setup data
   file <- paste0(tempdir(), "/test.csv")
   data <- data.frame("a" = c("2020-01-01", "2020-01-02"), "b" = 1:2)
   write.csv(data, file)
@@ -36,22 +19,4 @@ test_that("read of data works", {
   expect_equal(out$time_series, data$b)
   expect_null(out$T_star)
   expect_equal(out$controls, controls)
-
-  ### HHMM
-  file <- paste0(tempdir(), "/dax.csv")
-  sink(tempfile())
-  download_data(symbol = "^GDAXI", file = file)
-  sink()
-  controls <- list(
-    hierarchy = TRUE,
-    horizon = c(100, NA),
-    period = "m",
-    data = list(
-      file = c(file, file),
-      data_column = c("Close", "Close"),
-      to = "2021-10-01"
-    )
-  )
-  controls <- set_controls(controls)
-  expect_snapshot(read_data(controls))
 })
