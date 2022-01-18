b3b0390fcc30446c485ec0b8bb041237b58e7678
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-01-18 17:12:04 +0100

    Use tinytex instead.

--- jss/workflow.R
+++ jss/workflow.R
@@ -1,5 +1,5 @@
 try(setwd("jss"),silent=TRUE)
 Sweave("fhmm_oelschlaeger_adam_michels.Rnw")
-tools::texi2pdf("fhmm_oelschlaeger_adam_michels.tex")
+tinytex::pdflatex("fhmm_oelschlaeger_adam_michels.tex")
 Stangle("fhmm_oelschlaeger_adam_michels.Rnw")
 
