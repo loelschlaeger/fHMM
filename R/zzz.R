02b4e1a0c749e533cf1d5abdb67c3b54f6ded983
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-01-22 06:51:59 +0100

    Updated start-up message.

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,8 @@
 .onAttach <- function(lib, pkg) {
   # startup message
   msg <- c(
-    paste0(
-      "Thanks for using fHMM version ", utils::packageVersion("fHMM")
-    ), ", have fun!\n",
-    "See https://loelschlaeger.github.io/fHMM for help.\n",
+    paste0("Thanks for using fHMM version ", utils::packageVersion("fHMM")), "!\n",
+    "See https://loelschlaeger.de/fHMM for help.\n",
     "Type 'citation(\"fHMM\")' for citing this R package."
   )
   packageStartupMessage(msg)
