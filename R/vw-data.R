b7c34855c400c774f7fed281ebbc16e393f0720f
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-01-29 21:01:08 +0100

    Added datasets.

---
+++ R/vw-data.R
@@ -0,0 +1,17 @@
+#' VW stock time series
+#'
+#' @docType data
+#'
+#' @usage data(vw)
+#'
+#' @format
+#' A data frame containing prices and volumes of the VW stock with corresponding dates.
+#'
+#' @source
+#' The data was obtained via the \code{download_data()} function from 
+#' \url{https://finance.yahoo.com} on 2022-01-29.
+#'
+#' @keywords
+#' dataset
+#'
+'vw'
