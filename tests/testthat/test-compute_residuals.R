a830efe55accfce04fbd332d9b389ddc5a324226
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2022-02-10 21:16:05 +0000

    Style :lipstick:

--- tests/testthat/test-compute_residuals.R
+++ tests/testthat/test-compute_residuals.R
@@ -1,6 +1,6 @@
 test_that("residual computation works", {
   data(dax_model)
-  x = compute_residuals(dax_model, verbose = FALSE)
+  x <- compute_residuals(dax_model, verbose = FALSE)
   expect_snapshot(x)
   expect_snapshot(unclass(x))
 })
