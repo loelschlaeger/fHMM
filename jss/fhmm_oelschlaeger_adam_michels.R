4d2ca7965195af465f1a7783dc6cf6ed31f590eb
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-04-04 13:32:32 +0200

    Wrote section "data management".

--- jss/fhmm_oelschlaeger_adam_michels.R
+++ jss/fhmm_oelschlaeger_adam_michels.R
@@ -27,7 +27,7 @@ class(controls)
 ###################################################
 controls <- list(
   states  = 2,
-  sdds    = "gamma(mu = -1|1)",
+  sdds    = "gamma(mu = 0.5|2)",
   horizon = 500,
   fit     = list(runs = 50)
 )
@@ -47,14 +47,20 @@ set_controls(controls)
 
 
 ###################################################
-### code chunk number 5: get data
+### code chunk number 5: download dax example
+###################################################
+download_data(symbol = "^GDAXI", from = "2000-01-01", to = Sys.Date())
+
+
+###################################################
+### code chunk number 6: get data
 ###################################################
 system.file("extdata", "dax.csv", package = "fHMM")
 system.file("extdata", "vw.csv", package = "fHMM")
 
 
 ###################################################
-### code chunk number 6: prepare_data example
+### code chunk number 7: prepare_data example
 ###################################################
 controls <- list(
   states = 3,
@@ -66,17 +72,17 @@ controls <- list(
 )
 controls <- set_controls(controls)
 data <- prepare_data(controls)
+class(data)
+
+
+###################################################
+### code chunk number 8: fHMM_data summary
+###################################################
 summary(data)
 
 
 ###################################################
-### code chunk number 7: download dax example
-###################################################
-download_data(symbol = "^GDAXI", from = "2000-01-01", to = Sys.Date())
-
-
-###################################################
-### code chunk number 8: ts
+### code chunk number 9: ts
 ###################################################
 events <- fHMM_events(
   list(
@@ -85,12 +91,40 @@ events <- fHMM_events(
                "First COVID-19 case in Germany")
     )
   )
-print(events)
 plot(data, events = events)
 
 
 ###################################################
-### code chunk number 9: data preparation
+### code chunk number 10: revisited controls example
+###################################################
+controls <- list(
+  states  = 2,
+  sdds    = "gamma(mu = 0.5|2)",
+  horizon = 500,
+  fit     = list(runs = 50)
+)
+controls <- set_controls(controls)
+
+
+###################################################
+### code chunk number 11: fHMM_parameters example
+###################################################
+pars <- fHMM_parameters(
+  controls = controls, Gamma = matrix(c(0.9,0.2,0.1,0.8), nrow = 2), 
+  sigmas = c(0.1,0.5)
+)
+class(pars)
+
+
+###################################################
+### code chunk number 12: simdata
+###################################################
+data <- prepare_data(controls, true_parameters = pars, seed = 1)
+plot(data)
+
+
+###################################################
+### code chunk number 13: data preparation
 ###################################################
 controls <- list(
   states = 3,
@@ -108,86 +142,86 @@ data <- prepare_data(controls)
 
 
 ###################################################
-### code chunk number 10: access model
+### code chunk number 14: access model
 ###################################################
 data(dax_model_3t)
 
 
 ###################################################
-### code chunk number 11: summarize model
+### code chunk number 15: summarize model
 ###################################################
 summary(dax_model_3t)
 
 
 ###################################################
-### code chunk number 12: sdds
+### code chunk number 16: sdds
 ###################################################
 plot(dax_model_3t, plot_type = "sdds")
 
 
 ###################################################
-### code chunk number 13: ll
+### code chunk number 17: ll
 ###################################################
 plot(dax_model_3t, plot_type = "ll")
 
 
 ###################################################
-### code chunk number 14: load dax model
+### code chunk number 18: load dax model
 ###################################################
 data(dax_model_3t)
 
 
 ###################################################
-### code chunk number 15: dax decode states
+### code chunk number 19: dax decode states
 ###################################################
 dax_model_3t <- decode_states(dax_model_3t)
 
 
 ###################################################
-### code chunk number 16: dec_ts
+### code chunk number 20: dec_ts
 ###################################################
 plot(dax_model_3t)
 
 
 ###################################################
-### code chunk number 17: reorder states
+### code chunk number 21: reorder states
 ###################################################
 dax_model_3t <- reorder_states(dax_model_3t, 3:1)
 
 
 ###################################################
-### code chunk number 18: predict
+### code chunk number 22: predict
 ###################################################
 predict(dax_model_3t, ahead = 10)
 
 
 ###################################################
-### code chunk number 19: load dax data
+### code chunk number 23: load dax data
 ###################################################
 data(dax_model_3t)
 
 
 ###################################################
-### code chunk number 20: compute residuals
+### code chunk number 24: compute residuals
 ###################################################
 dax_model_3t <- compute_residuals(dax_model_3t)
 
 
 ###################################################
-### code chunk number 21: residuals
+### code chunk number 25: residuals
 ###################################################
 plot(dax_model_3t, plot_type = "pr")
 
 
 ###################################################
-### code chunk number 22: jb test
+### code chunk number 26: jb test
 ###################################################
 res <- dax_model_3t$residuals
 tseries::jarque.bera.test(res)
 
 
 ###################################################
-### code chunk number 23: compare models
+### code chunk number 27: compare models
 ###################################################
 data(dax_model_2n)
 data(dax_model_3t)
