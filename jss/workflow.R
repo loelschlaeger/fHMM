962b02fe941f6b21dfb75139da13dd6658b190a5
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2022-07-06 10:22:43 +0200

    Update of ORCID and replication material.

--- jss/workflow.R
+++ jss/workflow.R
@@ -1,5 +1,4 @@
 try(setwd("jss"),silent=TRUE)
 Sweave("fhmm_oelschlaeger_adam_michels.Rnw")
 tinytex::pdflatex("fhmm_oelschlaeger_adam_michels.tex")
-Stangle("fhmm_oelschlaeger_adam_michels.Rnw")
-
+# Stangle("fhmm_oelschlaeger_adam_michels.Rnw")
