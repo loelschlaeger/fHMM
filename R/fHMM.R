5693f0168c8dcf850e5d0e4b04b547b0605fc2c8
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2022-12-10 11:41:06 +0100

    updated startup message

--- R/fHMM.R
+++ R/fHMM.R
@@ -14,3 +14,17 @@
 #' @keywords
 #' internal
 NULL
+
+#' @noRd
+#' @importFrom cli style_hyperlink
+#' @importFrom utils packageVersion
+
+.onAttach <- function(lib, pkg) {
+  msg <- c(
+    paste0("Thanks for using {fHMM} version ", utils::packageVersion("fHMM")), "!\n",
+    "See ", cli::style_hyperlink("https://loelschlaeger.de/fHMM", "https://loelschlaeger.de/fHMM") ," for help.\n",
+    "Type 'citation(\"fHMM\")' for citing this R package."
+  )
+  packageStartupMessage(msg)
+  invisible()
+}
